5306f3073aadfd3a6b4a2dcb594e7c2b05157b3c
Danny Bail <danny.bail0407@gmail.com>
2017-06-21 11:01:51 +0100

    IdentifyType complete, added to do list

--- vip_code/plot_v_ln_T.py
+++ vip_code/plot_v_ln_T.py
@@ -1,6 +1,8 @@
 # A program to generate plots of v and ln(T/T_c) against xi.
 # May in future split into two files, one containing the program itself, and a toolbox-type file.
 
+# To do: vPlus/vMinus functions, find eqn for cs from EIKR EoS, try to identify xi_end (see findTminus) mathematically
+# rather than guessing
 import numpy as np
 
 
@@ -15,11 +17,22 @@ def min_speed_deton(al_p, cs):
 #     return 1/(3*vPlus(vm, al_p, 'Deflagration'))
 
 
-def IdentifyType(vWall, al_p, cs):
-    if vWall<cs:
+def IdentifyType(vw, al_p, cs):
+    # vw = wall velocity, al_p is alpha plus, cs is speed of sound (varies dependent and EoS used).
+    # vPlus and vMinus are functions based on 2016 work, to be clarified with Mark.
+    if vw < cs:
         wallType = 'Def'
-    elif vWall>cs:
-        if vWall<min_speed_deton(al_p, cs):
+        vm = vw
+        vp = vPlus(vm)
+    elif vw > cs:
+        if vw < min_speed_deton(al_p, cs):
             wallType = 'Hyb'
         else:
             wallType = 'Det'
+            vp = vw
+            vm = vMinus(vp)
+    # Should consider case where vWall==cs
+    print 'Using wall type ', wallType
+    print 'v- = ', vm
+    print 'v+  = ', vp
+    return wallType, vp, vm
