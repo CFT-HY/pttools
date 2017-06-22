ea1461fe2c7e068bdfd58ee4a000c48aa0b5c35b
Danny Bail <danny.bail0407@gmail.com>
2017-06-22 17:12:14 +0100

    vPlus, vMinus functions defined

--- vip_code/plot_v_ln_T.py
+++ vip_code/plot_v_ln_T.py
@@ -2,19 +2,19 @@
 # May in future split into two files, one containing the program itself, and a toolbox-type file.
 
 # To do:
-# vPlus/vMinus functions
 # Try to identify xi_end (see findTminus) mathematically rather than guessing
 # -> Find expression for cs minus (Espinosa pg 10)
+# 0<alphaPlus<1/3
 import numpy as np
 import EIKR_Toolbox as eikr
 
 
-def min_speed_deton(al_p, cs):
+def min_speed_deton(al_p, c):
     # Minimum speed for a detonation
-    return (cs/(1 + al_p))*(1 + np.sqrt(al_p*(2. + 3.*al_p)))
+    return (c/(1 + al_p))*(1 + np.sqrt(al_p*(2. + 3.*al_p)))
 
 
-# def max_speed_deflag(al_p, cs):
+# def max_speed_deflag(al_p, c):
 #     # Maximum speed for a deflagration
 #     vm=cs
 #     return 1/(3*vPlus(vm, al_p, 'Deflagration'))
@@ -22,7 +22,6 @@ def min_speed_deton(al_p, cs):
 
 def IdentifyType(vw, al_p, c):
     # vw = wall velocity, al_p is alpha plus, cs is speed of sound (varies dependent and EoS used).
-    # vPlus and vMinus are functions based on 2016 work, to be clarified with Mark.
     if vw < c:
         wallType = 'Def'
         vm = vw
@@ -43,12 +42,29 @@ def IdentifyType(vw, al_p, c):
     return wallType, vp, vm
 
 
-def xi_stop(wallType, vw):
-    if wallType == 'Def':
-        xs = vw
-    else:
-        xs = csminus()
-    return xs
+def vPlus(vm, al_p):
+    if vm < 1./np.sqrt(3.):
+        vp = (1./(1.+al_p))*(((vm/2.)+(1./6.*vm))-np.sqrt(((vm/2.)+(1./6.*vm))**2+(al_p**2)+((2./3.)*al_p)-(1./3.)))
+    elif vm > 1./np.sqrt(3.):
+        vp = (1./(1.+al_p))*(((vm/2.)+(1./6.*vm))+np.sqrt(((vm/2.)+(1./6.*vm))**2+(al_p**2)+((2./3.)*al_p)-(1./3.)))
+    # Edge case?
+    return vp
+
+
+def vMinus(vp, al_p):
+    if vp < 1./np.sqrt(3.):
+        vm = (((1+al_p)/2.)*vp+((1.-3.*al_p)/6*vp))-np.sqrt((((1.+al_p)/2.)*vp+((1.-3.*al_p)/6.*vp))**2-(1./3.))
+    elif vp > 1./np.sqrt(3.):
+        vm = (((1+al_p)/2.)*vp+((1.-3.*al_p)/6*vp))+np.sqrt((((1.+al_p)/2.)*vp+((1.-3.*al_p)/6.*vp))**2-(1./3.))
+    # Edge case?
+    return vm
+
+# def xi_stop(wallType, vw):
+#     if wallType == 'Def':
+#         xs = vw # wrong side, want xi shock
+#     else:
+#         xs = csMinus() # T dep, don't know final T
+#     return xs
 
 
 def cs(T, state):
