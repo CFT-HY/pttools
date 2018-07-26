8581d1d1d7ba3039b81fe1666c46d709c2487514
DBail <danny.bail@gmail.com>
2018-07-26 18:12:00 +0100

    Model_comparison now plots difference between models

--- vip_code/model_comparison.py
+++ vip_code/model_comparison.py
@@ -4,6 +4,7 @@ import new_general_shooting_solution as gss
 import EIKR_Toolbox as Eikr
 import Bag_Toolbox as Bag
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def bagify():
@@ -18,7 +19,21 @@ def bagify():
     return
 
 
-# def plot_diff(true_xi, true_w, true_v, e_xi, e_w, e_v):
+def plot_diff(xi, true_w, true_v, e_w, e_v, j, xi_wall):
+    diff_w = true_w - e_w
+    diff_v = true_v - e_v
+    plt.figure(2*j)
+    plt.plot(xi, diff_w)
+    plt.title(r'$\xi_w=${}'.format(xi_wall))
+    plt.xlabel(r'$\xi$')
+    plt.ylabel(r'$w_{true}-w_{eikr}$')
+    plt.figure(2*j+1)
+    plt.plot(xi, diff_v)
+    plt.title(r'$\xi_w=${}'.format(xi_wall))
+    plt.xlabel(r'$\xi$')
+    plt.ylabel(r'$v_{true}-v_{eikr}$')
+    return
+
 
 def plot_over(true_xi, true_w, true_v, e_xi, e_w, e_v, j, xi_wall):
     plt.figure(2*j)
@@ -45,24 +60,40 @@ def plot_over(true_xi, true_w, true_v, e_xi, e_w, e_v, j, xi_wall):
     return
 
 
-# def line_extend(xi, w, v):
+def merge(true_xi, true_w, true_v, e_xi, e_w, e_v):
+    # Since the xi values of the two models do not line up, this function interpolates the data sets to make 'complete'
+    # ones which can be more easily compared
+    full_xi = np.append(true_xi, e_xi)
+    full_xi = np.sort(full_xi)
+    # print(full_xi)
+    full_true_w = np.interp(full_xi, true_xi[::-1], true_w[::-1])
+    full_true_v = np.interp(full_xi, true_xi[::-1], true_v[::-1])
+    full_e_w = np.interp(full_xi, e_xi[::-1], e_w[::-1])
+    full_e_v = np.interp(full_xi, e_xi[::-1], e_v[::-1])
+    return full_xi, full_true_w, full_true_v, full_e_w, full_e_v
 
 
+Bag.print_params()
 true_bag_xi_1, true_bag_w_1, true_bag_v_1 = gss.plot_graph_module(0.4, Bag)
-true_bag_xi_2, true_bag_w_2, true_bag_v_2 = gss.plot_graph_module(0.45, Bag)
+# true_bag_xi_2, true_bag_w_2, true_bag_v_2 = gss.plot_graph_module(0.45, Bag)
 # true_bag_xi_3, true_bag_w_3, true_bag_v_3 = gss.plot_graph_module(0.5, Bag)
 # true_bag_xi_4, true_bag_w_4, true_bag_v_4 = gss.plot_graph_module(0.55, Bag)
 # Bag.print_params()
 # print('')
 bagify()
 print('')
+Bag.print_params()
 # Bag.print_params()
 e_bag_xi_1, e_bag_w_1, e_bag_v_1 = gss.plot_graph_module(0.4, Bag)
-e_bag_xi_2, e_bag_w_2, e_bag_v_2 = gss.plot_graph_module(0.45, Bag)
+# e_bag_xi_2, e_bag_w_2, e_bag_v_2 = gss.plot_graph_module(0.45, Bag)
 # e_bag_xi_3, e_bag_w_3, e_bag_v_3 = gss.plot_graph_module(0.5, Bag)
 # e_bag_xi_4, e_bag_w_4, e_bag_v_4 = gss.plot_graph_module(0.55, Bag)
 
 
 plot_over(true_bag_xi_1, true_bag_w_1, true_bag_v_1, e_bag_xi_1, e_bag_w_1, e_bag_v_1, 1, 0.4)
-plot_over(true_bag_xi_2, true_bag_w_2, true_bag_v_2, e_bag_xi_2, e_bag_w_2, e_bag_v_2, 2, 0.45)
+m_xi_1, m_true_w_1, m_true_v_1, m_e_w_1, m_e_v_1 = merge(true_bag_xi_1, true_bag_w_1, true_bag_v_1, e_bag_xi_1,
+                                                         e_bag_w_1, e_bag_v_1)
+plot_diff(m_xi_1, m_true_w_1, m_true_v_1, m_e_w_1, m_e_v_1, 10, 0.4)
 plt.show()
+
+# plot_over(true_bag_xi_2, true_bag_w_2, true_bag_v_2, e_bag_xi_2, e_bag_w_2, e_bag_v_2, 2, 0.45)
