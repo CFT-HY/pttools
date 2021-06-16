ff6767eefe65bd0884012885f726ffc35328071d
Mika Mäki <agentti002x@gmail.com>
2021-06-16 18:06:18 +0300

    Enable logging for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,18 +1,18 @@
-# import logging
-# import os
-# import time
-#
-# log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs")
-# if not os.path.isdir(log_dir):
-#     os.mkdir(log_dir)
-#
-# log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}")
-#
-# logging.basicConfig(
-#     handlers=[
-#         logging.FileHandler(log_file_path),
-#         logging.StreamHandler()
-#     ],
-#     level=logging.DEBUG,
-#     format='%(asctime)s %(levelname)-8s %(module)-16s %(message)s'
-# )
+import logging
+import os
+import time
+
+log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs")
+if not os.path.isdir(log_dir):
+    os.mkdir(log_dir)
+
+log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}_{os.getpid()}")
+
+logging.basicConfig(
+    handlers=[
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler()
+    ],
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)-8s %(module)-16s %(funcName)-32s %(lineno)-4d %(message)s'
+)
