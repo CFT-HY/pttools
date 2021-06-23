27a09ac9a24327775da795c361cb614902f1a42b
Mika Mäki <agentti002x@gmail.com>
2021-06-23 10:27:21 +0300

    Add txt file extension for logs

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__)
 if not os.path.isdir(log_dir):
     os.mkdir(log_dir)
 
-log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}_{os.getpid()}")
+log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}_{os.getpid()}.txt")
 
 logging.basicConfig(
     handlers=[
