49b279e708539c3b6710d5ecd6350908378fb193
Mika Mäki <agentti002x@gmail.com>
2021-07-02 14:58:49 +0300

    Add more detail to Numba type hints

--- pttools/type_hints.py
+++ pttools/type_hints.py
@@ -2,8 +2,9 @@
 
 import typing as tp
 
+from numba.core.registry import CPUDispatcher
 import numpy as np
 
 FLOAT_OR_ARR = tp.Union[float, np.ndarray]
-FLOAT_OR_ARR_NUMBA = tp.Union[float, np.ndarray, callable]
+FLOAT_OR_ARR_NUMBA = tp.Union[float, np.ndarray, callable, CPUDispatcher]
 INT_OR_ARR = tp.Union[int, np.ndarray]
