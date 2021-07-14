e68317be103131119747e08f0b33f31289043777
Mika Mäki <agentti002x@gmail.com>
2021-07-14 16:23:57 +0300

    Use test_utils.assert_allclose() in test_speedup.py

--- tests/test_speedup.py
+++ tests/test_speedup.py
@@ -3,14 +3,15 @@ import unittest
 import numpy as np
 
 from pttools import speedup
+from . import test_utils
 
 
 class TestSpeedup(unittest.TestCase):
     @staticmethod
     def test_gradient():
         arr = np.logspace(1, 5, 10)
-        np.testing.assert_allclose(speedup.gradient(arr), np.gradient(arr))
+        test_utils.assert_allclose(speedup.gradient(arr), np.gradient(arr))
 
     @staticmethod
     def test_logspace():
-        np.testing.assert_allclose(speedup.logspace(1, 5, 10), np.logspace(1, 5, 10))
+        test_utils.assert_allclose(speedup.logspace(1, 5, 10), np.logspace(1, 5, 10))
