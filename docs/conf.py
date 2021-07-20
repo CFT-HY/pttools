d26b03aa79f8314a573f8c6f2fb1e2f5bd8a7079
Mika Mäki <agentti002x@gmail.com>
2021-07-20 10:24:07 +0300

    Add Numba to the intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,7 @@ extlinks: tp.Dict[str, tp.Tuple[str, tp.Optional[str]]] = {
 intersphinx_mapping: tp.Dict[str, tp.Tuple[str, tp.Optional[str]]] = {
     "cobaya": ("https://cobaya.readthedocs.io/en/latest/", None),
     "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numba": ("https://numba.readthedocs.io/en/stable/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None)
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
 }
