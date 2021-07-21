439818265aa52594b5064a52b141f249027d814b
Mika Mäki <agentti002x@gmail.com>
2021-07-21 16:29:20 +0300

    Improve code quality for the relativistic combustion figure

--- docs/fig/relativistic_combustion.py
+++ docs/fig/relativistic_combustion.py
@@ -1,89 +1,128 @@
-# Import modules
-import sys
+"""Script for generating a figure of the three different types of relativistic combustion
 
-sys.path.insert(0, "/Users/dacuttin/Projects/pttools/bubble/")
-import bubble as b
-import numpy as np
+Original version was developed by Daniel Cutting for the figure 14 of the :notes:`lecture notes <>`.
+"""
+
+import typing as tp
+
 import matplotlib
 import matplotlib.pyplot as plt
-
-## setup latex plotting
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-## make font size bigger
-matplotlib.rcParams.update({'font.size': 28})
-## but make legend smaller
-matplotlib.rcParams.update({'legend.fontsize': 14})
-## change line thickness
-matplotlib.rcParams.update({'lines.linewidth': 1.75})
+import matplotlib.cm
+import matplotlib.colors
+from matplotlib.image import AxesImage
+import numpy as np
 from scipy.interpolate import interp1d
 
-import seaborn as sb
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
-from matplotlib import cm
+from pttools import bubble
 
-fig, ax = plt.subplots(1, 3, figsize=(27, 9))
+_VIRIDIS_BIG = matplotlib.cm.get_cmap("autumn_r", 512)
+_NEW_COLORS = _VIRIDIS_BIG(np.linspace(0, 1, 256))
+_NEW_COLORS[0] = matplotlib.colors.to_rgba("white", alpha=0)
+COLORMAP = matplotlib.colors.ListedColormap(_NEW_COLORS)
 
-alpha = 0.5
-vws = [0.44, 0.72, 0.92]
-plot_cbar = [False, False, True]
-viridisBig = cm.get_cmap('autumn_r', 512)
 
-newcolors = viridisBig(np.linspace(0, 1, 256))
-newcolors[0] = matplotlib.colors.to_rgba('white', alpha=0)
-newcmp = ListedColormap(newcolors)
+def setup_matplotlib(**kwargs):
+    matplotlib.rcParams.update({
+        "text.usetex": True,
+        "font.family": "serif",
+        "font.size": 28,
+        "legend.fontsize": 14,
+        "lines.linewidth": 1.75,
+        **kwargs
+    })
 
-Np = 5000
 
-label_s = [r'subsonic deflagration' + '\n' + r'$v_\mathrm{w} \leq c_s$',
-           r'supersonic deflagration' + '\n' + r'$c_s<v_\mathrm{w} < c_\mathrm{J}$',
-           r'detonation' + '\n' + r'$c_s<c_\mathrm{J}\leq v_\mathrm{w}$']
-print(label_s)
-for i in range(len(vws)):
-
-    v_f, enthalp, xi = b.fluid_shell(vws[i], alpha, Np)
-    n_wall = b.find_v_index(xi, vws[i])
+def plot_bubble(ax: plt.Axes, label: str, v_wall: float, alpha: float, n_xi: int) -> AxesImage:
+    v_f, enthalp, xi = bubble.fluid_shell(v_wall, alpha, n_xi)
+    n_wall = bubble.find_v_index(xi, v_wall)
     v_fluid = interp1d(xi, v_f, fill_value=0, bounds_error=False)
 
-    xvalues = np.linspace(-1.5 * xi[n_wall], 1.5 * xi[n_wall], num=4000)
-    yvalues = np.linspace(-1.5 * xi[n_wall], 1.5 * xi[n_wall], num=4000)
-
+    xi_wall = xi[n_wall]
+    xvalues = np.linspace(-1.5 * xi_wall, 1.5 * xi_wall, num=4000)
+    yvalues = np.linspace(-1.5 * xi_wall, 1.5 * xi_wall, num=4000)
     xxgrid, yygrid = np.meshgrid(xvalues, yvalues)
 
-    fluid_grid = v_fluid(np.sqrt((xxgrid) * (xxgrid) + (yygrid) * (yygrid)))
-
+    fluid_grid = v_fluid(np.sqrt(xxgrid * xxgrid + yygrid * yygrid))
     fluid_grid = fluid_grid / np.max(fluid_grid)
+    arrow_width = 0.03 * xi_wall
 
-    arrow_width = 0.03 * xi[n_wall]
+    cs = ax.imshow(
+        fluid_grid,
+        cmap=COLORMAP,
+        extent=(-1.5 * xi_wall, 1.5 * xi_wall, -1.5 * xi_wall, 1.5 * xi_wall),
+        interpolation="bilinear")
+    circle = plt.Circle((0, 0), xi_wall, color="k", linewidth=4, fill=None)
+    ax.arrow(
+        0.75 * xi_wall, 0, 0.5 * xi_wall, 0,
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        0. * xi_wall, 0.75 * xi_wall, 0, 0.5 * xi_wall,
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        -0.75 * xi_wall, 0, -0.5 * xi_wall, 0,
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        0. * xi_wall, -0.75 * xi_wall, 0, -0.5 * xi_wall,
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        (0.75 * xi_wall) / np.sqrt(2),
+        (0.75 * xi_wall) / np.sqrt(2),
+        (0.5 * xi_wall) / np.sqrt(2),
+        (0.5 * xi_wall) / np.sqrt(2),
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        -(0.75 * xi_wall) / np.sqrt(2),
+        (0.75 * xi_wall) / np.sqrt(2),
+        -(0.5 * xi_wall) / np.sqrt(2),
+        (0.5 * xi_wall) / np.sqrt(2),
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        (0.75 * xi_wall) / np.sqrt(2),
+        -(0.75 * xi_wall) / np.sqrt(2),
+        (0.5 * xi_wall) / np.sqrt(2),
+        -(0.5 * xi_wall) / np.sqrt(2),
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
+    ax.arrow(
+        -(0.75 * xi_wall) / np.sqrt(2),
+        -(0.75 * xi_wall) / np.sqrt(2),
+        -(0.5 * xi_wall) / np.sqrt(2),
+        -(0.5 * xi_wall) / np.sqrt(2),
+        shape="full", width=arrow_width, edgecolor="k", facecolor="k")
 
-    cs = ax[i].imshow(fluid_grid, cmap=newcmp,
-                      extent=(-1.5 * xi[n_wall], 1.5 * xi[n_wall], -1.5 * xi[n_wall], 1.5 * xi[n_wall]),
-                      interpolation='bilinear')
-    circle = plt.Circle((0, 0), xi[n_wall], color='k', linewidth=4, fill=None)
-    ax[i].arrow(0.75 * xi[n_wall], 0, 0.5 * xi[n_wall], 0, shape='full', width=arrow_width, edgecolor='k',
-                facecolor='k')
-    ax[i].arrow(0. * xi[n_wall], 0.75 * xi[n_wall], 0, 0.5 * xi[n_wall], shape='full', width=arrow_width, edgecolor='k',
-                facecolor='k')
-    ax[i].arrow(-0.75 * xi[n_wall], 0, -0.5 * xi[n_wall], 0, shape='full', width=arrow_width, edgecolor='k',
-                facecolor='k')
-    ax[i].arrow(0. * xi[n_wall], -0.75 * xi[n_wall], 0, -0.5 * xi[n_wall], shape='full', width=arrow_width,
-                edgecolor='k', facecolor='k')
-    ax[i].arrow((0.75 * xi[n_wall]) / np.sqrt(2), (0.75 * xi[n_wall]) / np.sqrt(2), (0.5 * xi[n_wall]) / np.sqrt(2),
-                (0.5 * xi[n_wall]) / np.sqrt(2), shape='full', width=arrow_width, edgecolor='k', facecolor='k')
-    ax[i].arrow(-(0.75 * xi[n_wall]) / np.sqrt(2), (0.75 * xi[n_wall]) / np.sqrt(2), -(0.5 * xi[n_wall]) / np.sqrt(2),
-                (0.5 * xi[n_wall]) / np.sqrt(2), shape='full', width=arrow_width, edgecolor='k', facecolor='k')
-    ax[i].arrow((0.75 * xi[n_wall]) / np.sqrt(2), -(0.75 * xi[n_wall]) / np.sqrt(2), (0.5 * xi[n_wall]) / np.sqrt(2),
-                -(0.5 * xi[n_wall]) / np.sqrt(2), shape='full', width=arrow_width, edgecolor='k', facecolor='k')
-    ax[i].arrow(-(0.75 * xi[n_wall]) / np.sqrt(2), -(0.75 * xi[n_wall]) / np.sqrt(2), -(0.5 * xi[n_wall]) / np.sqrt(2),
-                -(0.5 * xi[n_wall]) / np.sqrt(2), shape='full', width=arrow_width, edgecolor='k', facecolor='k')
+    ax.add_artist(circle)
+    ax.axis("off")
+    ax.annotate(label, (0.51, -0.1), xycoords="axes fraction", ha="center", va="center", fontsize=30)
+    return cs
 
-    if plot_cbar[i] == True:
-        cbar = fig.colorbar(cs, ax=ax)
-        cbar.set_label(r'$v/v_\mathrm{peak}$')
-    ax[i].add_artist(circle)
-    ax[i].axis('off')
 
-    ax[i].annotate(label_s[i], (0.51, -0.1), xycoords='axes fraction', ha='center', va='center', fontsize=30)
+def main(
+        alpha: float = 0.5,
+        v_walls: tp.Tuple[float, ...] = (0.44, 0.72, 0.92),
+        plot_cbars: tp.Tuple[bool, ...] = (False, False, True),
+        n_xi: int = 5000,
+        figsize: tp.Tuple[int, int] = (27, 9)) -> plt.Figure:
+    setup_matplotlib()
 
-plt.show()
-fig.savefig('plots/all_circle.pdf', bbox_inches='tight')
+    fig: plt.Figure
+    axs: np.ndarray
+    fig, axs = plt.subplots(1, len(v_walls), figsize=figsize)
+
+    labels = [
+        "subsonic deflagration" + "\n" + r"$v_\mathrm{w} \leq c_s$",
+        "supersonic deflagration" + "\n" + r"$c_s<v_\mathrm{w} < c_\mathrm{J}$",
+        "detonation" + "\n" + r"$c_s<c_\mathrm{J}\leq v_\mathrm{w}$"
+    ]
+
+    for ax, label, v_wall, plot_cbar in zip(axs, labels, v_walls, plot_cbars):
+        cs = plot_bubble(ax, label, v_wall, alpha, n_xi)
+        if plot_cbar:
+            cbar = fig.colorbar(cs, ax=axs)
+            cbar.set_label(r"$v/v_\mathrm{peak}$")
+
+    # fig.savefig("plots/all_circle.pdf", bbox_inches="tight")
+    return fig
+
+
+if __name__ == "__main__":
+    main()
+    plt.show()
