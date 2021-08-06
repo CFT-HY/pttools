2cc443409d06ce4a328c33d098436b5227bd8914
Mika Mäki <agentti002x@gmail.com>
2021-08-06 12:47:08 +0300

    Add documentation for SSMtools constants

--- pttools/ssmtools/const.py
+++ pttools/ssmtools/const.py
@@ -7,9 +7,12 @@ from pttools import bubble
 # TODO: Use typing.Final, when the oldest supported Python version is 3.8
 # https://www.python.org/dev/peps/pep-0591/
 
-NXIDEFAULT: int = 2000  # Default number of xi points used in bubble profiles
-NTDEFAULT: int = 200    # Default number of T-tilde values for bubble lifetime distribution integration
-NQDEFAULT: int = 320    # Default number of wavevectors used in the velocity convolution integrations.
+#: Default number of xi points used in bubble profiles
+NXIDEFAULT: int = 2000
+#: Default number of T-tilde values for bubble lifetime distribution integration
+NTDEFAULT: int = 200
+#: Default number of wavevectors used in the velocity convolution integrations.
+NQDEFAULT: int = 320
 NPT_TYPE = tp.Union[np.ndarray, tp.Tuple[int, int, int]]
 NPTDEFAULT: NPT_TYPE = (NXIDEFAULT, NTDEFAULT, NQDEFAULT)
 
@@ -20,15 +23,19 @@ NPTDEFAULT: NPT_TYPE = (NXIDEFAULT, NTDEFAULT, NQDEFAULT)
 #
 # NTDEFAULT can be left as it is, or even reduced to 100
 
-# Default dimensionless wavenumber above which to use approximation for sin_transform, sin_transform_approx.
+#: Default dimensionless wavenumber above which to use approximation for sin_transform, sin_transform_approx.
 # TODO: check that this can actually be a float
 Z_ST_THRESH: float = 50
 
-DZ_ST_BLEND: float = np.pi  # Default wavenumber overlap for matching sin_transform_approx
+#: Default wavenumber overlap for matching sin_transform_approx
+DZ_ST_BLEND: float = np.pi
 
-T_TILDE_MAX: float = 20.0   # Maximum in bubble lifetime distribution integration
-T_TILDE_MIN: float = 0.01   # Minimum in bubble lifetime distribution integration
+#: Maximum in bubble lifetime distribution integration
+T_TILDE_MAX: float = 20.0
+#: Minimum in bubble lifetime distribution integration
+T_TILDE_MIN: float = 0.01
 
 DEFAULT_NUC_PARM: tp.Tuple[int] = (1,)
 
-CS0: np.float_ = bubble.CS0  # Default sound speed
+#: Default sound speed
+CS0: np.float_ = bubble.CS0
