674d0d7542973bc3633fdf90d9b25ef32d4ba32d
Mika Mäki <agentti002x@gmail.com>
2022-02-14 19:37:03 +0200

    Add a note for the dependency of cs2 functions on Phase

--- pttools/bubble/boundary.py
+++ pttools/bubble/boundary.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 @enum.unique
 class Phase(float, enum.Enum):
     """In general the phase is a scalar variable (a real number), and therefore also these values are floats."""
+    # Do not change these values without also checking the model cs2 functions.
     SYMMETRIC = 0.
     BROKEN = 1.
 
