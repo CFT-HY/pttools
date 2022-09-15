74357a48b1a8c1067bc11ed2a0e4a94d392cef79
Mika Mäki <agentti002x@gmail.com>
2022-09-15 21:53:47 +0300

    Expand TestBag tests to cover ConstCSLikeBag

---
+++ tests/models/base_bag.py
@@ -0,0 +1,35 @@
+import abc
+
+import numpy as np
+
+from pttools.bubble.boundary import Phase
+from tests.models.base_model import ModelBaseCase
+from tests.utils.assertions import assert_allclose
+
+
+class BagBaseCase(ModelBaseCase, abc.ABC):
+    """Test that a model corresponds to the bag model"""
+    #: This test should use the bag model reference data instead of creating its own
+    SAVE_NEW_DATA = False
+
+    def test_alphas_same(self):
+        r""""The two definitions of the transition strength coincide
+        only in the case of detonations within the bag model."
+
+        See :notes:` \` p. 40
+        """
+        wn = 1.1
+        alpha_n = self.model.alpha_n(wn=wn)
+        alpha_plus = self.model.alpha_plus(wp=wn, wm=0.9)
+        self.assertAlmostEqual(alpha_n, alpha_plus)
+
+    def test_cs2_like_bag(self):
+        """Test that cs2 = 1/3"""
+        assert_allclose(self.model.cs2(self.w_arr1, self.phase_arr), 1 / 3 * np.ones_like(self.w_arr1))
+
+    def test_theta_constant(self):
+        """The theta of the bag model is a constant"""
+        theta_s = self.model.theta(self.w_arr1, Phase.SYMMETRIC)
+        theta_b = self.model.theta(self.w_arr1, Phase.BROKEN)
+        assert_allclose(theta_s, np.ones_like(self.w_arr1)*self.model.V_s, atol=1e-16)
+        assert_allclose(theta_b, np.ones_like(self.w_arr1)*self.model.V_b, atol=1e-16)
--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,41 +1,20 @@
 import unittest
 
-import numpy as np
-
-from pttools.bubble.boundary import Phase
 from pttools import models
 from tests.models.base_model import ModelBaseCase
-from tests.utils.assertions import assert_allclose
+from tests.models.base_bag import BagBaseCase
 
 
-class TestBag(ModelBaseCase, unittest.TestCase):
+class TestBag(BagBaseCase, unittest.TestCase):
+    SAVE_NEW_DATA = True
+
     @classmethod
     def setUpClass(cls, *args, **kwargs) -> None:
         model = models.BagModel(a_s=1.2, a_b=1.1, V_s=1.3)
         super().setUpClass(model)
 
-    def test_alphas_same(self):
-        r""""The two definitions of the transition strength coincide
-        only in the case of detonations within the bag model."
 
-        See :notes:` \` p. 40
-        """
-        wn = 1.1
-        alpha_n = self.model.alpha_n(wn=wn)
-        alpha_plus = self.model.alpha_plus(wp=wn, wm=0.9)
-        self.assertAlmostEqual(alpha_n, alpha_plus)
-
-    def test_theta_constant(self):
-        """The theta of the bag model is a constant"""
-        theta_s = self.model.theta(self.w_arr1, Phase.SYMMETRIC)
-        theta_b = self.model.theta(self.w_arr1, Phase.BROKEN)
-        assert_allclose(theta_s, np.ones_like(self.w_arr1)*self.model.V_s, atol=1e-16)
-        assert_allclose(theta_b, np.ones_like(self.w_arr1)*self.model.V_b, atol=1e-16)
-
-
-class TestConstCSLikeBag(ModelBaseCase, unittest.TestCase):
-    #: This test should use the bag model reference data instead of creating its own
-    SAVE_NEW_DATA = False
+class TestConstCSLikeBag(BagBaseCase, unittest.TestCase):
     model: models.ConstCSModel
 
     @classmethod
@@ -52,9 +31,6 @@ class TestConstCSLikeBag(ModelBaseCase, unittest.TestCase):
     @unittest.expectedFailure
     def test_critical_temp(self):
         super().test_critical_temp()
-
-    def test_cs2_like_bag(self):
-        assert_allclose(self.model.cs2(self.w_arr1, self.phase_arr), 1/3*np.ones_like(self.w_arr1))
 
 
 class TestConstCS(ModelBaseCase, unittest.TestCase):
