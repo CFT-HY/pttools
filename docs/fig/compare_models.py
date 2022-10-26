Fix sign in ConstCSModel.const_cs_wn_const

docs/fig/compare_models.py
@@ -60,7 +60,7 @@ def main():
 
     comp = ModelComparison(
         temp=np.linspace(1, 100, 100),
-        phase=models.Phase.BROKEN
+        phase=models.Phase.SYMMETRIC
     )
     comp.add(model_bag)
     comp.add(model_thermo_bag, ls="--")

docs/fig/v_chapman_jouguet.py
@@ -46,15 +46,15 @@ def main():
     # Plot opacity
     alpha = 1
     # Transition strength
-    alpha_n = np.linspace(0, 0.3, n_points)
+    alpha_n = np.linspace(0.15, 0.3, n_points)
     # alpha_n = np.linspace(0.07, 2, n_points)
 
     model_bag = models.BagModel(a_s=1.1, a_b=1, V_s=1)
     model_const_cs_like_bag = models.ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1)
     model_const_cs = models.ConstCSModel(a_s=1.1, a_b=1, css2=0.25, csb2=1/3, V_s=1)
-    model_const_cs2 = models.ConstCSModel(a_s=1.1, a_b=1, css2=1/4, csb2=1/3.5, V_s=1)
+    model_const_cs2 = models.ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=0.25, V_s=1)
     model_const_cs_full = models.FullModel(
-        thermo=models.ConstCSThermoModel(a_s=1.1, a_b=1, css2=0.25, csb2=1/3, V_s=1)
+        thermo=models.ConstCSThermoModel(a_s=1.1, a_b=1, css2=1/3, csb2=0.25, V_s=1)
     )
     # print("w_n:", model_const_cs.w_n(alpha_n=0.1))
     # print("alpha_n:", model_const_cs.alpha_n(wn=1e6))
@@ -94,12 +94,12 @@ def main():
 
     # debug_plot(ax1, model_bag)
     temp = np.linspace(1, 10, 100)
-    ax3.plot(temp, model_const_cs.e_temp(temp, bubble.Phase.BROKEN)/temp**4, label="analytical e")
-    ax3.plot(temp, model_const_cs.p_temp(temp, bubble.Phase.BROKEN)/temp**4, label="analytical p")
-    ax3.plot(temp, model_const_cs.w(temp, bubble.Phase.BROKEN) / temp ** 4, label="analytical w")
-    ax3.plot(temp, model_const_cs_full.e_temp(temp, bubble.Phase.BROKEN)/temp**4, label="full e")
-    ax3.plot(temp, model_const_cs_full.p_temp(temp, bubble.Phase.BROKEN)/temp**4, label="full p")
-    ax3.plot(temp, model_const_cs_full.w(temp, bubble.Phase.BROKEN) / temp ** 4, label="full w")
+    ax3.plot(temp, model_const_cs.e_temp(temp, bubble.Phase.SYMMETRIC)/temp**4, label="analytical e")
+    ax3.plot(temp, model_const_cs.p_temp(temp, bubble.Phase.SYMMETRIC)/temp**4, label="analytical p")
+    ax3.plot(temp, model_const_cs.w(temp, bubble.Phase.SYMMETRIC) / temp ** 4, label="analytical w")
+    ax3.plot(temp, model_const_cs_full.e_temp(temp, bubble.Phase.SYMMETRIC)/temp**4, label="full e")
+    ax3.plot(temp, model_const_cs_full.p_temp(temp, bubble.Phase.SYMMETRIC)/temp**4, label="full p")
+    ax3.plot(temp, model_const_cs_full.w(temp, bubble.Phase.SYMMETRIC) / temp ** 4, label="full w")
     # print(model_const_cs_full.V(bubble.Phase.BROKEN))
     ax3.set_xlabel("T")
     ax3.set_ylabel("e, p, w / T**4")
@@ -121,8 +121,8 @@ def main():
     )
     ax2.plot(alpha_n, v_cj_const_cs, label=r"Constant $c_s$ model", alpha=alpha, ls="--")
     ax2.plot(alpha_n, v_cj_const_cs_like_bag, label=r"Constant $c_s$ model with bag coeff.", alpha=alpha, ls=":")
-    ax2.plot(alpha_n, v_cj_const_cs2, label=r"Constant $c_s$ model v2", alpha=alpha, ls="--")
-    ax2.plot(alpha_n, v_cj_const_cs_full, label=r"Constant $c_s$ ThermoModel", alpha=alpha)
+    ax2.plot(alpha_n, v_cj_const_cs2, label=r"Constant $c_s$ model v2", alpha=alpha, ls="-")
+    ax2.plot(alpha_n, v_cj_const_cs_full, label=r"Constant $c_s$ ThermoModel", alpha=alpha, ls=":")
     ax2.set_xlabel(r"$\alpha_n$")
     ax2.set_ylabel("$v_{CJ}$")
 

pttools/models/thermo.py
@@ -62,7 +62,7 @@ class ThermoModel(BaseModel, abc.ABC):
             err.append("cannot exceed 1")
         if err:
             msg = ", ".join(err)
-            logger.error(f"Invalid {name}: {msg}, got range: {np.min(cs2)} - {np.max(cs2)}")
+            logger.error(f"Invalid {name} for {self.name}: {msg}, got range: {np.min(cs2)} - {np.max(cs2)}")
             return False
         return True
 
@@ -71,6 +71,8 @@ class ThermoModel(BaseModel, abc.ABC):
         cs2_b = self.cs2_full(self.GEFF_DATA_TEMP, Phase.BROKEN)
         self.validate_cs2(cs2_s, "cs2_s")
         self.validate_cs2(cs2_b, "cs2_b")
+        # with np.printoptions(threshold=np.inf):
+        #     print(np.array([self.GEFF_DATA_TEMP, cs2_s]).T)
 
         cs2_spl_s = scipy.interpolate.splrep(
             np.log10(self.GEFF_DATA_TEMP),
@@ -148,7 +150,10 @@ class ThermoModel(BaseModel, abc.ABC):
 
     def cs2_full(self, temp: th.FloatOrArr, phase: th.FloatOrArr) -> th.FloatOrArr:
         """Full evaluation of $c_s^2$ from the underlying quantities"""
-        return self.dp_dt(temp, phase) / self.de_dt(temp, phase)
+        # This hopefully reduces numerical errors
+        return (self.dgp_dT(temp, phase)*temp + 4*self.gp(temp, phase)) / \
+               (3 * (self.dge_dT(temp, phase)*temp + 4*self.ge(temp, phase)))
+        # return self.dp_dt(temp, phase) / self.de_dt(temp, phase)
 
     def dgp_dT(self, temp: th.FloatOrArr, phase: th.FloatOrArr) -> th.FloatOrArr:
         return 4*self.dgs_dT(temp, phase) - 3*self.dge_dT(temp, phase)

tests/models/base_model.py
@@ -18,7 +18,7 @@ class ModelBaseCase(JsonTestCase, abc.ABC):
     REF_DATA_PATH: str
     TEST_ARR_SIZE: int = 10
 
-    alpha_n = np.linspace(0.05, 0.5, 10)
+    alpha_n = np.linspace(0.15, 0.5, 10)
     temp_arr = np.linspace(1, 100, TEST_ARR_SIZE)
     w_arr1 = temp_arr**4
     w_arr2 = temp_arr**3.9