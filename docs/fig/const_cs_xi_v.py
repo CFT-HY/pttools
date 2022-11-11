e94a569e377dec41dda414e4e8e38664d1c0d4dc
Mika Mäki <agentti002x@gmail.com>
2022-11-11 17:44:40 +0200

    Improve const_cs_xi_v plot

--- docs/fig/const_cs_xi_v.py
+++ docs/fig/const_cs_xi_v.py
@@ -14,15 +14,15 @@ from tests.paper.plot_plane import plot_plane
 
 
 def main():
-    model = ConstCSModel(a_s=1.1, a_b=1, css2=0.4**2, csb2=1/3, V_s=1, V_b=0)
+    cs = 1 / np.sqrt(3) - 0.05
+
+    model = ConstCSModel(a_s=1.1, a_b=1, css2=cs**2, csb2=1/3, V_s=1, V_b=0)
+    # model = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1 / 3, V_s=1, V_b=0)
     df_dtau_ptr = add_df_dtau("const_cs", model.cs2)
     data_b, data_s = xiv_plane(df_dtau_ptr=df_dtau_ptr, cs2_s=model.css2, cs2_b=model.csb2)
     fig: plt.Figure = plt.figure()
     ax: plt.Axes = fig.add_subplot()
     plot_plane(ax=ax, data_b=data_b, data_s=data_s, cs2_s=model.css2, cs2_b=model.csb2, selected_solutions=False)
-
-    xi = np.linspace(0.4, 1, 20)
-    ax.plot(xi, lorentz(xi, 0.4), c="r", ls="-.", label=r"$\mu(\xi,c_{sb}=0.4)$")
 
     model2 = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1, V_b=0)
     v, w, xi = fluid_shell_generic(model2, v_wall=0.85, alpha_n=0.05, sol_type=SolutionType.DETON)
@@ -34,12 +34,16 @@ def main():
     v, w, xi = fluid_shell(v_wall=0.5, alpha_n=0.578)
     ax.plot(xi, v, c="g", ls=":")
 
-    # model3 = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=0.4**2, V_s=1, V_b=0)
-    model3 = ConstCSModel(a_s=1.1, a_b=1, css2=0.4**2, csb2=1/3, V_s=1, V_b=0)
+    xi = np.linspace(cs, 1, 20)
+    ax.plot(xi, lorentz(xi, cs), c="r", ls="-.", label=rf"$\mu(\xi,c_{{sb}}={cs:.3f})$")
+
+    ax.axvline(cs, c="r", ls=":")
+    ax.axhline(cs, c="r", ls=":")
+    model3 = ConstCSModel(a_s=1.1, a_b=1, css2=cs**2, csb2=cs**2, V_s=1, V_b=0)
     v, w, xi = fluid_shell_generic(model3, v_wall=0.95, alpha_n=0.15, sol_type=SolutionType.DETON)
-    ax.plot(xi, v, c="r", label=r"$c_{sb}=0.4$")
+    ax.plot(xi, v, c="r", label=rf"$c_{{s}}={cs:.3f}$")
     v, w, xi = fluid_shell_generic(
-        model3, v_wall=0.39, alpha_n=0.6, sol_type=SolutionType.SUB_DEF, reverse=False, allow_failure=True)
+        model3, v_wall=0.4, alpha_n=0.6, sol_type=SolutionType.SUB_DEF, reverse=False, allow_failure=True)
     ax.plot(xi, v, c="r")
 
     ax.legend()
