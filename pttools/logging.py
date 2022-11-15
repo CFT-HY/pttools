63603f0bedfa60d7c982a28f43727ec2e6acbd0a
Mika Mäki <agentti002x@gmail.com>
2022-11-15 16:06:18 +0200

    Move logging configuration from tests to pttools

---
+++ pttools/logging.py
@@ -0,0 +1,26 @@
+import faulthandler
+import logging
+import os
+import time
+
+
+def setup_logging(log_dir: str = None, enable_faulthandler: bool = True, silence_spam: bool = True):
+    if enable_faulthandler and not faulthandler.is_enabled():
+        faulthandler.enable()
+
+    if log_dir is None:
+        log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs")
+    os.makedirs(log_dir, exist_ok=True)
+    log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}_{os.getpid()}.txt")
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler(log_file_path),
+            logging.StreamHandler()
+        ],
+        level=logging.DEBUG,
+        # level=logging.INFO,
+        format='%(asctime)s %(levelname)-8s %(module)-20s %(funcName)-32s %(lineno)-4d %(message)s'
+    )
+    if silence_spam:
+        logging.getLogger("matplotlib").setLevel(logging.INFO)
+        logging.getLogger("numba").setLevel(logging.INFO)
--- tests/__init__.py
+++ tests/__init__.py
@@ -5,28 +5,7 @@ These tests are not part of PTtools itself and are not included in the PTtools p
 However, they contain good examples on how to use PTtools and its dependencies.
 """
 
-import faulthandler
-import logging
-import os
-import time
+from pttools.logging import setup_logging
 
-if not faulthandler.is_enabled():
-    faulthandler.enable()
 
-log_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "logs")
-os.makedirs(log_dir, exist_ok=True)
-
-log_file_path = os.path.join(log_dir, f"tests_{time.strftime('%Y-%m-%d_%H-%M-%S')}_{os.getpid()}.txt")
-
-logging.basicConfig(
-    handlers=[
-        logging.FileHandler(log_file_path),
-        logging.StreamHandler()
-    ],
-    level=logging.DEBUG,
-    # level=logging.INFO,
-    format='%(asctime)s %(levelname)-8s %(module)-16s %(funcName)-32s %(lineno)-4d %(message)s'
-)
-# Disable log spam
-logging.getLogger("matplotlib").setLevel(logging.INFO)
-logging.getLogger("numba").setLevel(logging.INFO)
+setup_logging()
