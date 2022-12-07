edaa75700db7a77df6b256b5ec74a4b305ac0479
Mika Mäki <agentti002x@gmail.com>
2022-12-07 18:13:17 +0200

    Add tests for thermodynamics

---
+++ tests/bubble/test_bubble.py
@@ -0,0 +1,49 @@
+import numpy as np
+import unittest
+
+from pttools.models.bag import BagModel
+from pttools.bubble.bubble import Bubble
+
+
+class BubbleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        model = BagModel(a_s=1.1, a_b=1, V_s=1)
+        cls.bubble = Bubble(model, v_wall=0.5, alpha_n=0.1)
+        cls.bubble.solve()
+
+    def test_ebar(self):
+        ebar = self.bubble.ebar
+        self.assertGreater(ebar, 0)
+
+    @unittest.expectedFailure
+    def test_kappa(self):
+        kappa = self.bubble.kappa
+        self.assertGreater(kappa, 0)
+        self.assertLess(kappa, 1)
+
+    def test_kinetic_energy_fraction(self):
+        kef = self.bubble.kinetic_energy_fraction
+        self.assertGreater(kef, 0)
+        self.assertLess(kef, 1)
+
+    def test_kinetic_energy_density(self):
+        ked = self.bubble.kinetic_energy_density
+        self.assertGreater(ked, 0)
+
+    def test_mean_adiabatic_index(self):
+        mabi = self.bubble.mean_adiabatic_index
+        self.assertGreater(mabi, 0)
+
+    def test_omega(self):
+        omega = self.bubble.omega
+        self.assertGreater(omega, 0)
+        self.assertLess(omega, 1)
+
+    def test_thermal_energy_density(self):
+        ted = self.bubble.thermal_energy_density
+        self.assertGreater(ted, 0)
+
+    def test_trace_anomaly(self):
+        trace_anomaly = self.bubble.trace_anomaly
+        self.assertTrue(np.isfinite(trace_anomaly))
