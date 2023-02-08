40d84a35858a51dead2ada49599076c52db1827e
Mika Mäki <agentti002x@gmail.com>
2023-02-08 11:37:50 +0200

    Fix default worker count for macOS

--- pttools/speedup/options.py
+++ pttools/speedup/options.py
@@ -1,13 +1,23 @@
 """Options for JIT-compilation and other speedups"""
 
 import logging
+import multiprocessing
 import os
 import typing as tp
 
 logger = logging.getLogger(__name__)
 
 #: Maximum workers for ProcessPoolExecutor (determined dynamically based on the available CPUs)
-MAX_WORKERS_DEFAULT: int = len(os.sched_getaffinity(0))
+if hasattr(os, "sched_getaffinity"):
+    # This is available only on some platforms
+    MAX_WORKERS_DEFAULT: int = len(os.sched_getaffinity(0))
+else:
+    # multiprocessing.cpu_count() is a wrapper around os.cpu_count()
+    # https://stackoverflow.com/a/53537394
+    logger.debug(
+        "This platform does not provide info on which CPU cores are available for this process. Using all cores."
+    )
+    MAX_WORKERS_DEFAULT: int = multiprocessing.cpu_count()
 
 #: Whether Numba JIT compilation has been disabled.
 NUMBA_DISABLE_JIT: tp.Final[bool] = bool(int(os.getenv("NUMBA_DISABLE_JIT", "0")))
