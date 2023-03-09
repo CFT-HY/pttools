33f5576fd3204f18594c335e483d8e5e89eb485a
Mika Mäki <agentti002x@gmail.com>
2023-03-09 17:30:45 +0200

    Improve fluid reference generation

--- pttools/bubble/fluid_reference.py
+++ pttools/bubble/fluid_reference.py
@@ -8,6 +8,7 @@ import h5py
 import numpy as np
 
 from pttools.bubble import boundary
+from pttools.bubble.alpha import alpha_n_max_bag
 from pttools.bubble.fluid_bag import fluid_shell
 from pttools.bubble.relativity import lorentz
 from pttools.speedup.parallel import run_parallel
@@ -22,9 +23,9 @@ class FluidReference:
             v_wall_min: float = 0.05,
             v_wall_max: float = 0.95,
             alpha_n_min: float = 0.05,
-            alpha_n_max: float = 0.05,
-            n_v_wall: int = 20,
-            n_alpha_n: int = 20):
+            alpha_n_max: float = 0.95,
+            n_v_wall: int = 18,
+            n_alpha_n: int = 18):
         self.path = path
         self.v_wall = np.linspace(v_wall_min, v_wall_max, n_v_wall, endpoint=True)
         self.alpha_n = np.linspace(alpha_n_min, alpha_n_max, n_alpha_n, endpoint=True)
@@ -48,25 +49,33 @@ class FluidReference:
         logger.info("Generating fluid reference")
         if os.path.exists(self.path):
             os.remove(self.path)
-        with h5py.File(self.path, "w") as file:
-            params = np.empty((self.v_wall.size, self.alpha_n.size, 2))
-            params[:, :, 0], params[:, :, 1] = np.meshgrid(self.v_wall, self.alpha_n)
+        try:
+            with h5py.File(self.path, "w") as file:
+                alpha_n_max = alpha_n_max_bag(self.v_wall)
 
-            vp, vm, vp_tilde, vm_tilde, wp, wm = run_parallel(
-                compute,
-                params,
-                multiple_params=True,
-                unpack_params=True,
-                output_dtypes=(np.float_, np.float_, np.float_, np.float_, np.float_, np.float_),
-                log_progress=True
-            )
-            file.create_dataset("vp", data=vp)
-            file.create_dataset("vm", data=vm)
-            file.create_dataset("vp_tilde", data=vp_tilde)
-            file.create_dataset("vm_tilde", data=vm_tilde)
-            file.create_dataset("wp", data=wp)
-            file.create_dataset("wm", data=wm)
-            # file.create_dataset("wn", data=wn)
+                params = np.empty((self.v_wall.size, self.alpha_n.size, 3))
+                params[:, :, 0], params[:, :, 1] = np.meshgrid(self.v_wall, self.alpha_n)
+                params[:, :, 2], _ = np.meshgrid(alpha_n_max, self.alpha_n)
+
+                vp, vm, vp_tilde, vm_tilde, wp, wm = run_parallel(
+                    compute,
+                    params,
+                    multiple_params=True,
+                    unpack_params=True,
+                    output_dtypes=(np.float_, np.float_, np.float_, np.float_, np.float_, np.float_),
+                    log_progress=True
+                )
+                file.create_dataset("vp", data=vp)
+                file.create_dataset("vm", data=vm)
+                file.create_dataset("vp_tilde", data=vp_tilde)
+                file.create_dataset("vm_tilde", data=vm_tilde)
+                file.create_dataset("wp", data=wp)
+                file.create_dataset("wm", data=wm)
+                # file.create_dataset("wn", data=wn)
+        except Exception as e:
+            # Remove broken file
+            os.remove(self.path)
+            raise e
         logger.info("Fluid reference ready")
 
     def get(self, v_wall: float, alpha_n: float) -> np.ndarray:
@@ -75,8 +84,15 @@ class FluidReference:
         return self.data[i_v_wall, i_alpha_n, :]
 
 
-def compute(v_wall: float, alpha_n: float) -> tp.Tuple[float, float, float, float, float, float]:
+def compute(v_wall: float, alpha_n: float, alpha_n_max: float) -> tp.Tuple[float, float, float, float, float, float]:
+    if alpha_n > alpha_n_max:
+        return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
+
     v, w, xi = fluid_shell(v_wall, alpha_n)
+    if np.any(np.isnan(v)) or np.any(np.isnan(w)) or np.any(np.isnan(xi)):
+        logger.error("Got nan values from the integration at v_wall=%s, alpha_n=%s", v_wall, alpha_n)
+        return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
+
     i_wall = np.argmax(v)
     i_wall_w = np.argmax(w)
     if i_wall != i_wall_w:
@@ -87,10 +103,10 @@ def compute(v_wall: float, alpha_n: float) -> tp.Tuple[float, float, float, floa
     vp = v[i_wall]
     vm = v[i_wall-1]
     vp_tilde = lorentz(v_wall, vp)
-    if vp_tilde < 0:
+    if np.isnan(vp_tilde) or vp_tilde < 0:
         raise ValueError(f"vp={vp}, vp_tilde={vp_tilde}")
     vm_tilde = lorentz(v_wall, vm)
-    if vm_tilde < 0:
+    if np.isnan(vm_tilde) or vm_tilde < 0:
         raise ValueError(f"vm={vm}, vm_tilde={vm_tilde}")
     wp = w[i_wall]
     wm = w[i_wall-1]
@@ -100,8 +116,10 @@ def compute(v_wall: float, alpha_n: float) -> tp.Tuple[float, float, float, floa
         raise ValueError(f"The old solver should always have wn=1, got wn={wn}")
 
     dev = boundary.junction_condition_deviation1(vp_tilde, wp, vm_tilde, wm)
-    if not np.isclose(dev, 0, atol=9e-3):
-        raise ValueError(f"Deviation from boundary conditions: {dev}")
+    if not np.isclose(dev, 0, atol=0.025):
+        logger.warning(f"Deviation from boundary conditions: %s at v_wall=%s, alpha_n=%s", dev, v_wall, alpha_n)
+        if not np.isclose(dev, 0, atol=0.025):
+            return np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
     return vp, vm, vp_tilde, vm_tilde, wp, wm
 
 
@@ -113,3 +131,9 @@ def ref():
             "to ensure that each process doesn't have to load it separately. "
             "Call this function once before creating sub-processes.")
     return FluidReference(path=os.path.join(os.path.dirname(__file__), "fluid_reference.hdf5"))
+
+
+if __name__ == "__main__":
+    from pttools.logging import setup_logging
+    setup_logging()
+    ref()
