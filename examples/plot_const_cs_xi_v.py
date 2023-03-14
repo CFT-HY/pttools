b34982f2672f507ce0ca248f00301c8de6eaf019
Mika Mäki <agentti002x@gmail.com>
2023-03-14 18:44:07 +0200

    Use Bubble in examples instead of fluid_shell_generic

--- examples/plot_const_cs_xi_v.py
+++ examples/plot_const_cs_xi_v.py
@@ -8,7 +8,7 @@ Plot the $\xi, v$ plane for the constant sound speed model
 import matplotlib.pyplot as plt
 import numpy as np
 
-from pttools.bubble.fluid import fluid_shell_generic
+from pttools.bubble.bubble import Bubble
 from pttools.bubble.fluid_bag import fluid_shell
 from pttools.bubble.boundary import SolutionType
 from pttools.bubble.integrate import add_df_dtau
@@ -30,13 +30,14 @@ def main():
     plot_plane(ax=ax, data_b=data_b, data_s=data_s, cs2_s=model.css2, cs2_b=model.csb2, selected_solutions=False)
 
     model2 = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1, V_b=0)
-    v, w, xi, sol_type, _, _, failed = fluid_shell_generic(model2, v_wall=0.85, alpha_n=0.05, sol_type=SolutionType.DETON)
-    ax.plot(xi, v, c="b", label=r"$c_{sb}=\frac{1}{\sqrt{3}}$")
+    det2 = Bubble(model2, v_wall=0.85, alpha_n=0.05, sol_type=SolutionType.DETON)
+    det2.solve()
+    ax.plot(det2.xi, det2.v, c="b", label=r"$c_{sb}=\frac{1}{\sqrt{3}}$")
     v, w, xi = fluid_shell(v_wall=0.85, alpha_n=0.05)
     ax.plot(xi, v, c="g", label="bag", ls=":")
-    v, w, xi, sol_type, _, _, failed = fluid_shell_generic(
-        model2, v_wall=0.5, alpha_n=0.578, sol_type=SolutionType.SUB_DEF, reverse=False)
-    ax.plot(xi, v, c="b")
+    def2 = Bubble(model2, v_wall=0.5, alpha_n=0.578, sol_type=SolutionType.SUB_DEF)
+    def2.solve()
+    ax.plot(def2.xi, def2.v, c="b")
     v, w, xi = fluid_shell(v_wall=0.5, alpha_n=0.578)
     ax.plot(xi, v, c="g", ls=":")
 
@@ -46,11 +47,14 @@ def main():
     ax.axvline(cs, c="r", ls=":")
     ax.axhline(cs, c="r", ls=":")
     model3 = ConstCSModel(a_s=1.1, a_b=1, css2=cs**2, csb2=cs**2, V_s=1, V_b=0)
-    v, w, xi, sol_type, _, _, failed = fluid_shell_generic(model3, v_wall=0.95, alpha_n=0.15, sol_type=SolutionType.DETON)
-    ax.plot(xi, v, c="r", label=rf"$c_{{s}}={cs:.3f}$")
-    v, w, xi, sol_type, _, _, failed = fluid_shell_generic(
-        model3, v_wall=0.4, alpha_n=0.6, sol_type=SolutionType.SUB_DEF, reverse=False, allow_failure=True)
-    ax.plot(xi, v, c="r")
+    det3 = Bubble(model3, v_wall=0.95, alpha_n=0.15, sol_type=SolutionType.DETON)
+    det3.solve()
+    ax.plot(det3.xi, det3.v, c="r", label=rf"$c_{{s}}={cs:.3f}$")
+    # This can be expected to fail due to the shape of the curve
+    def3 = Bubble(
+        model3, v_wall=0.4, alpha_n=0.6)
+    def3.solve()
+    ax.plot(def3.xi, def3.v, c="r")
 
     ax.legend()
     return fig
