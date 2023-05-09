4ecb37996d4d79a3d8158220ef751a4578b9b868
Mika Mäki <agentti002x@gmail.com>
2023-05-09 16:34:58 +0300

    Add preliminary Standard Model figures

--- examples/entropy/plot_entropy.py
+++ examples/entropy/plot_entropy.py
@@ -15,6 +15,8 @@ from pttools.analysis.plot_entropy import gen_and_plot_entropy
 from pttools.logging import setup_logging
 from pttools.models.bag import BagModel
 from pttools.models.const_cs import ConstCSModel
+from pttools.models.full import FullModel
+from pttools.models.sm import StandardModel
 from pttools import speedup
 
 from examples.utils import FIG_DIR
@@ -23,11 +25,13 @@ from tests.profiling import utils_cprofile
 
 def main():
     n_points = 10 if speedup.GITHUB_ACTIONS else 20
+    # sm = StandardModel(V_s=5e12, g_mult_s=1 + 1e-9)
     models = [
         # BagModel(a_s=1.1, a_b=1, V_s=1),
         # ConstCSModel(css2=1/3-0.01, csb2=1/3-0.02, a_s=1.5, a_b=1, V_s=1)
         BagModel(g_s=123, g_b=120, V_s=0.9),
         ConstCSModel(css2=1/3 - 0.01, csb2=1/3 - 0.011, g_s=123, g_b=120, V_s=0.9)
+        # FullModel(sm, t_crit_guess=100e3)
     ]
     alpha_n_min = np.max([model.alpha_n_min for model in models]) + 0.01
 
---
+++ examples/plot_standard_model_xi_v.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+from pttools.bubble.boundary import Phase
+from pttools.bubble.bubble import Bubble
+from pttools.logging import setup_logging
+from pttools.models.full import FullModel
+from pttools.models.sm import StandardModel
+
+
+setup_logging()
+
+sm = StandardModel(V_s=5e12, g_mult_s=1 + 1e-9)
+model = FullModel(sm, t_crit_guess=100e3)
+wn = model.w_n(alpha_n=0.1)
+tn = model.temp(wn, Phase.SYMMETRIC)
+print(wn, tn)
+bubble = Bubble(model, v_wall=0.3, alpha_n=0.05)
+bubble.solve()
+
+
+fig: plt.Figure = plt.figure()
+ax: plt.Axes = fig.add_subplot()
+
+ax.plot(bubble.xi, bubble.v)
+
+plt.show()
