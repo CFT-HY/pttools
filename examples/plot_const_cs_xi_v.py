7a463e4b8762e1151e0aa0988abcde8b8dac3c2f
Mika Mäki <mika.x.maki@helsinki.fi>
2023-07-07 16:09:05 +0300

    Improve plot_const_cs_xi_v

--- examples/plot_const_cs_xi_v.py
+++ examples/plot_const_cs_xi_v.py
@@ -19,17 +19,17 @@ from tests.paper.plane import xiv_plane
 from tests.paper.plot_plane import plot_plane
 
 
-# TODO: fix the location of the shock line
-
 def main():
-    cs = 1 / np.sqrt(3) - 0.05
+    css = 1 / np.sqrt(3) - 0.05
 
-    model = ConstCSModel(a_s=1.1, a_b=1, css2=cs**2, csb2=1/3, V_s=1, V_b=0)
+    fig: plt.Figure = plt.figure()
+    ax: plt.Axes = fig.add_subplot()
+
+    # Plane curves
+    model = ConstCSModel(a_s=1.1, a_b=1, css2=css**2, csb2=1/3, V_s=1, V_b=0)
     # model = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1, V_b=0)
     df_dtau_ptr = add_df_dtau("const_cs", model.cs2)
     data_b, data_s = xiv_plane(df_dtau_ptr=df_dtau_ptr, cs2_s=model.css2, cs2_b=model.csb2)
-    fig: plt.Figure = plt.figure()
-    ax: plt.Axes = fig.add_subplot()
     plot_plane(ax=ax, data_b=data_b, data_s=data_s, cs2_s=model.css2, cs2_b=model.csb2, selected_solutions=False)
 
     model2 = ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1, V_b=0)
@@ -44,15 +44,15 @@ def main():
     v, w, xi = fluid_shell(v_wall=0.5, alpha_n=0.578)
     ax.plot(xi, v, c="g", ls=":")
 
-    xi = np.linspace(cs, 1, 20)
-    ax.plot(xi, lorentz(xi, cs), c="r", ls="-.", label=rf"$\mu(\xi,c_{{sb}}={cs:.3f})$")
+    xi = np.linspace(css, 1, 20)
+    ax.plot(xi, lorentz(xi, css), c="r", ls="-.", label=rf"$\mu(\xi,c_{{sb}}={css:.3f})$")
 
-    ax.axvline(cs, c="r", ls=":")
-    ax.axhline(cs, c="r", ls=":")
-    model3 = ConstCSModel(a_s=1.1, a_b=1, css2=cs**2, csb2=cs**2, V_s=1, V_b=0)
+    ax.axvline(css, c="r", ls=":")
+    ax.axhline(css, c="r", ls=":")
+    model3 = ConstCSModel(a_s=1.1, a_b=1, css2=css**2, csb2=css**2, V_s=1, V_b=0)
     det3 = Bubble(model3, v_wall=0.95, alpha_n=0.15, sol_type=SolutionType.DETON)
     det3.solve()
-    ax.plot(det3.xi, det3.v, c="r", label=rf"$c_{{s}}={cs:.3f}$")
+    ax.plot(det3.xi, det3.v, c="r", label=rf"$c_{{s}}={css:.3f}$")
     # This can be expected to fail due to the shape of the curve
     def3 = Bubble(
         model3, v_wall=0.4, alpha_n=0.6)
