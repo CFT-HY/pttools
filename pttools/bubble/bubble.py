Fix average w calculation and naming of thermodynamic functions

pttools/bubble/bubble.py
@@ -179,9 +179,9 @@ class Bubble:
     def info_str(self, prec: str = ".4f") -> str:
         return \
             f"{self.label_unicode}: w0/wn={self.w[0] / self.wn:{prec}}, " \
-            f"Ubarf2={self.ubarf2:{prec}}, K={self.kinetic_energy_fraction:{prec}}, " \
+            f"Ubarf2={self.ubarf2:{prec}}, K={self.bva_kinetic_energy_fraction:{prec}}, " \
             f"κ={self.kappa:{prec}}, ω={self.omega:{prec}}, κ+ω={self.kappa + self.omega:{prec}}, " \
-            f"V-avg. trace anomaly={self.va_trace_anomaly:{prec}}"
+            f"V-avg. trace anomaly={self.va_trace_anomaly_diff:{prec}}"
 
     def solve(
             self,
@@ -245,17 +245,17 @@ class Bubble:
             logger.error(msg)
             self.add_note(msg)
             self.unphysical_alpha_plus = True
-        if self.va_entropy_density < 0:
+        if self.va_entropy_density_diff < 0:
             msg = "Entropy density should not be negative! Now entropy is decreasing. " \
-                  f"Got: {self.va_entropy_density} with " \
+                  f"Got: {self.va_entropy_density_diff} with " \
                   f"model={self.model.label_unicode}, v_wall={self.v_wall}, alpha_n={self.alpha_n}"
             if log_negative_entropy:
                 logger.warning(msg)
             self.add_note(msg)
             self.unphysical_entropy = True
-        if self.va_thermal_energy_density < 0:
+        if self.va_thermal_energy_density_diff < 0:
             msg = "Thermal energy density is negative. The bubble is therefore working as a heat engine. " \
-                  f"Got: {self.va_thermal_energy_density}"
+                  f"Got: {self.va_thermal_energy_density_diff}"
             logger.warning(msg)
             self.add_note(msg)
         if not np.isclose(self.kappa + self.omega, 1, rtol=sum_rtol_warning):
@@ -287,11 +287,13 @@ class Bubble:
     # Thermodynamics
     # -----
 
+    # bva = bubble volume averaged
+
     @functools.cached_property
     def bva_entropy_density(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.bva_entropy_density(self.model, self.w, self.xi, self.v_wall, self.phase)
+        return thermo.bva_entropy_density_diff(self.model, self.w, self.xi, self.v_wall, self.phase)
 
     @functools.cached_property
     def bva_entropy_density_relative(self) -> float:
@@ -306,10 +308,16 @@ class Bubble:
         return thermo.bva_kinetic_energy_density(self.v, self.w, self.xi, self.v_wall)
 
     @functools.cached_property
+    def bva_kinetic_energy_fraction(self) -> float:
+        if not self.solved:
+            raise NotYetSolvedError
+        return thermo.bva_kinetic_energy_fraction(ek_bva=self.bva_kinetic_energy_density, eb=self.ebar)
+
+    @functools.cached_property
     def bva_thermal_energy_density(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.bva_thermal_energy_density(self.w, self.xi, self.v_wall)
+        return thermo.bva_thermal_energy_density_diff(self.w, self.xi, self.v_wall)
 
     @functools.cached_property
     def bva_trace_anomaly(self) -> float:
@@ -327,14 +335,7 @@ class Bubble:
     def kappa(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.kappa(self.model, self.v, self.w, self.xi, self.v_wall, delta_e_theta=self.va_trace_anomaly)
-
-    @functools.cached_property
-    def kinetic_energy_fraction(self) -> float:
-        if not self.solved:
-            raise NotYetSolvedError
-        return thermo.kinetic_energy_fraction(
-            self.model, self.v, self.w, self.xi, ek=self.va_kinetic_energy_density)
+        return thermo.kappa(self.model, self.v, self.w, self.xi, self.v_wall, delta_e_theta=self.va_trace_anomaly_diff)
 
     @functools.cached_property
     def mean_adiabatic_index(self) -> float:
@@ -346,7 +347,7 @@ class Bubble:
     def omega(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.omega(self.model, self.w, self.xi, self.v_wall, delta_e_theta=self.va_trace_anomaly)
+        return thermo.omega(self.model, self.w, self.xi, self.v_wall, delta_e_theta=self.va_trace_anomaly_diff)
 
     @functools.cached_property
     def s(self):
@@ -365,31 +366,43 @@ class Bubble:
     # va = volume averaged
 
     @functools.cached_property
-    def va_entropy_density(self) -> float:
+    def va_entropy_density_diff(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.va_entropy_density(self.model, self.w, self.xi, self.v_wall, self.phase)
+        return thermo.va_entropy_density_diff(self.model, self.w, self.xi, self.v_wall, self.phase)
 
     @functools.cached_property
-    def va_entropy_density_relative(self) -> float:
+    def va_enthalpy_density(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return self.va_entropy_density / self.model.s(self.wn, Phase.SYMMETRIC)
+        return thermo.va_enthalpy_density(eq=self.bva_thermal_energy_density)
+
+    @functools.cached_property
+    def va_entropy_density_diff_relative(self) -> float:
+        if not self.solved:
+            raise NotYetSolvedError
+        return self.va_entropy_density_diff / self.model.s(self.wn, Phase.SYMMETRIC)
 
     @functools.cached_property
     def va_kinetic_energy_density(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.va_kinetic_energy_density(self.v, self.w, self.xi)
+        return thermo.va_kinetic_energy_density_diff(self.v, self.w, self.xi)
 
     @functools.cached_property
-    def va_thermal_energy_density(self) -> float:
+    def va_kinetic_energy_fraction(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
-        return thermo.va_thermal_energy_density(self.w, self.xi)
+        return thermo.va_kinetic_energy_fraction(ek_va=self.va_kinetic_energy_density, eb=self.ebar)
 
     @functools.cached_property
-    def va_trace_anomaly(self) -> float:
+    def va_thermal_energy_density_diff(self) -> float:
+        if not self.solved:
+            raise NotYetSolvedError
+        return thermo.va_thermal_energy_density_diff(self.w, self.xi)
+
+    @functools.cached_property
+    def va_trace_anomaly_diff(self) -> float:
         if not self.solved:
             raise NotYetSolvedError
         return thermo.va_trace_anomaly(self.model, self.w, self.xi, self.v_wall, self.phase)
@@ -399,9 +412,3 @@ class Bubble:
         if not self.solved:
             raise NotYetSolvedError
         return thermo.wbar(self.w, self.xi, self.v_wall, self.wn)
-
-    @functools.cached_property
-    def w_b(self) -> float:
-        if not self.solved:
-            raise NotYetSolvedError
-        return thermo.w_b(self.model, self.v, self.w, self.xi, v_wall=self.v_wall, phase=self.phase)

pttools/bubble/thermo.py
@@ -33,24 +33,38 @@ logger = logging.getLogger(__name__)
 # Todo: Fix the equations in the docstrings
 
 
-def bva_entropy_density(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
+def bva_entropy_density_diff(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
     r"""Bubble volume averaged entropy density
     $$\frac{3}{4\pi v_w^3} s_\text{avg}
     """
-    return 3/(4*np.pi * v_wall**3) * va_entropy_density(model, w, xi, v_wall, phase)
+    return 3/(4*np.pi * v_wall**3) * va_entropy_density_diff(model, w, xi, v_wall, phase)
 
 
 def bva_kinetic_energy_density(v: np.ndarray, w: np.ndarray, xi: np.ndarray, v_wall: float) -> float:
     r"""Bubble volume averaged kinetic energy density"""
-    return 3/(4*np.pi * v_wall**3) * va_kinetic_energy_density(v, w, xi)
+    return 3/(4*np.pi * v_wall**3) * va_kinetic_energy_density_diff(v, w, xi)
 
 
-def bva_thermal_energy_density(w: np.ndarray, xi: np.ndarray, v_wall: float) -> float:
+def bva_kinetic_energy_fraction(ek_bva: float, eb: float) -> float:
+    r"""Bubble volume averaged kinetic energy fraction
+    $$K_\text{bva} = \frac{e_{K,\text{bva}}}{\bar{e}}$$
+    """
+    return ek_bva / eb
+
+
+def bva_thermal_energy_density(v_wall: float, eqp: float) -> float:
+    r"""Volume-averaged thermal energy density after the phase transition
+    $$e_Q' = e_Q + e_\theta - e_K' - e_\theta' = 4\pi \int_0^{\xi_\text{max}} d\xi \xi^2 \frac{3}{4}w_n - e_K' - \Delta e_\theta$$
+    """
+    return 3/(4*np.pi * v_wall**3) * eqp
+
+
+def bva_thermal_energy_density_diff(w: np.ndarray, xi: np.ndarray, v_wall: float) -> float:
     r"""Bubble volume averaged thermal energy density"""
-    return 3/(4*np.pi * v_wall**3) * va_thermal_energy_density(w, xi)
+    return 3/(4*np.pi * v_wall**3) * va_thermal_energy_density_diff(w, xi)
 
 
-def bva_trace_anomaly(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
+def bva_trace_anomaly_diff(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
     r"""Bubble volume averaged trace anomaly
     $$\epsilon = \frac{3}{4\pi v_w^3} \Delta e_\theta$$
     """
@@ -69,25 +83,11 @@ def kappa(
         delta_e_theta: float = None) -> float:
     if delta_e_theta is None:
         delta_e_theta = va_trace_anomaly(model, w, xi, v_wall)
-    return va_kinetic_energy_density(v, w, xi) / np.abs(delta_e_theta)
+    return va_kinetic_energy_density_diff(v, w, xi) / np.abs(delta_e_theta)
 
 
 def kappa_approx(alpha_n: th.FloatOrArr) -> th.FloatOrArr:
     return alpha_n / (0.73 + 0.083*np.sqrt(alpha_n) + alpha_n)
-
-
-def kinetic_energy_fraction(
-        model: "Model",
-        v: np.ndarray, w: np.ndarray, xi: np.ndarray,
-        ek: float = None, eb: float = None) -> float:
-    r"""Kinetic energy fraction
-    $$K = \frac{e_K}{\bar{e}}$$
-    """
-    if ek is None:
-        ek = va_kinetic_energy_density(v, w, xi)
-    if eb is None:
-        eb = ebar(model, w[-1])
-    return ek / eb
 
 
 # @numba.njit
@@ -105,7 +105,7 @@ def omega(
         delta_e_theta: float = None) -> float:
     if delta_e_theta is None:
         delta_e_theta = va_trace_anomaly(model, w, xi, v_wall)
-    return va_thermal_energy_density(w, xi) / np.abs(delta_e_theta)
+    return va_thermal_energy_density_diff(w, xi) / np.abs(delta_e_theta)
 
 
 def ubarf2(v: np.ndarray, w: np.ndarray, xi: np.ndarray, v_wall: float, ek_bva: float = None) -> float:
@@ -131,7 +131,11 @@ def wbar(w: np.ndarray, xi: np.ndarray, v_wall: float, wn: float):
     # return ret
 
 
-def va_entropy_density(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
+def va_enthalpy_density(eq: float) -> float:
+    return 4/3 * eq
+
+
+def va_entropy_density_diff(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
     r"""
     Volume-averaged entropy density
     $$s_\text{avg} = \int d\xi \xi^2 (s(w,\phi) - s(w_n, \phi_s)$$
@@ -142,7 +146,7 @@ def va_entropy_density(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: fl
 
 
 # @numba.njit
-def va_kinetic_energy_density(v: np.ndarray, w: np.ndarray, xi: np.ndarray) -> float:
+def va_kinetic_energy_density_diff(v: np.ndarray, w: np.ndarray, xi: np.ndarray) -> float:
     r"""
     Volume-averaged kinetic energy density
     $$e_K = 4 \pi \int_0^{xi_\text{max}} d\xi \xi^2 w \gamma^2 v^2$$
@@ -157,8 +161,22 @@ def va_kinetic_energy_density(v: np.ndarray, w: np.ndarray, xi: np.ndarray) -> f
     return 4*np.pi/3 * np.trapz(w * v**2 * relativity.gamma2(v), xi**3)
 
 
+def va_kinetic_energy_fraction(ek_va: float, eb: float) -> float:
+    r"""Volume-averaged kinetic energy fraction
+    $$K_\text{va} = \frac{e_{K,\text{va}}}{\bar{e}}$$
+    """
+    return ek_va / eb
+
+
+def va_thermal_energy_density(v_shock: float, wn: float, ek: float, delta_e_theta: float) -> float:
+    r"""Volume-averaged thermal energy density after the phase transition
+    $$e_Q' = e_Q + e_\theta - e_K' - e_\theta' = 4\pi \int_0^{\xi_\text{max}} d\xi \xi^2 \frac{3}{4}w_n - e_K' - \Delta e_\theta$$
+    """
+    return np.pi * wn * v_shock**3 - ek - delta_e_theta
+
+
 # @numba.njit
-def va_thermal_energy_density(w: np.ndarray, xi: np.ndarray) -> float:
+def va_thermal_energy_density_diff(w: np.ndarray, xi: np.ndarray) -> float:
     r"""Volume-averaged thermal energy density
     $$\Delta e_Q = 4 \pi \int_0^{\xi_\text{max}} d\xi \xi^2 \frac{3}{4} (w - w_n)$$
     """
@@ -174,15 +192,3 @@ def va_trace_anomaly(model: "Model", w: np.ndarray, xi: np.ndarray, v_wall: floa
     theta = model.theta(w, phase)
     theta_n = model.theta(w[-1], Phase.SYMMETRIC)
     return 4*np.pi/3 * np.trapz((theta - theta_n), xi**3)
-
-
-def w_b(model: "Model", v: np.ndarray, w: np.ndarray, xi: np.ndarray, v_wall: float, phase: np.ndarray = None) -> float:
-    """"""
-    if phase is None:
-        phase = props.find_phase(xi, v_wall)
-    theta = model.theta(w, phase)
-    theta_n = model.theta(w[-1], Phase.SYMMETRIC)
-
-    i_max = np.nonzero(v)[0][-1]
-    # Todo: or i_max+1?
-    return 1/(xi[i_max]**3) * np.trapz(3/4 * w[-1] - w[:i_max]*relativity.gamma2(v[:i_max]) * v[:i_max]**2 + theta[:i_max] - theta_n, xi[:i_max]**3)

tests/bubble/test_bubble.py
@@ -22,8 +22,13 @@ class BubbleTest(unittest.TestCase):
         self.assertGreater(kappa, 0)
         self.assertLess(kappa, 1)
 
-    def test_kinetic_energy_fraction(self):
-        kef = self.bubble.kinetic_energy_fraction
+    def test_bva_kinetic_energy_fraction(self):
+        kef = self.bubble.bva_kinetic_energy_fraction
+        self.assertGreater(kef, 0)
+        self.assertLess(kef, 1)
+
+    def test_va_kinetic_energy_fraction(self):
+        kef = self.bubble.va_kinetic_energy_fraction
         self.assertGreater(kef, 0)
         self.assertLess(kef, 1)
 
@@ -41,9 +46,9 @@ class BubbleTest(unittest.TestCase):
         self.assertLess(omega, 1)
 
     def test_va_thermal_energy_density(self):
-        ted = self.bubble.va_thermal_energy_density
+        ted = self.bubble.va_thermal_energy_density_diff
         self.assertGreater(ted, 0)
 
     def test_va_trace_anomaly(self):
-        trace_anomaly = self.bubble.va_trace_anomaly
+        trace_anomaly = self.bubble.va_trace_anomaly_diff
         self.assertTrue(np.isfinite(trace_anomaly))

tests/bubble/test_thermo.py
@@ -17,7 +17,7 @@ class ThermoTest:
 
     KAPPA_REF: np.ndarray
     OMEGA_REF: np.ndarray
-    KE_FRAC_REF: np.ndarray
+    BVA_KE_FRAC_REF: np.ndarray
 
     bubbles: tp.List[Bubble]
 
@@ -41,9 +41,8 @@ class ThermoTest:
         """Ensure that there are no typos in the reference data"""
         assert_allclose(self.KAPPA_REF + self.OMEGA_REF, 1, 1.8e-2)
 
-    def test_ke_frac(self):
-        # Todo: get rid of this correcting factor and define ke_frac more rigorously!
-        assert_allclose([3/(4*np.pi * bubble.v_wall**3) * bubble.kinetic_energy_fraction for bubble in self.bubbles], self.KE_FRAC_REF, rtol=1.5e-2)
+    def test_bva_ke_frac(self):
+        assert_allclose([bubble.bva_kinetic_energy_fraction for bubble in self.bubbles], self.BVA_KE_FRAC_REF, rtol=1.5e-2)
 
     def test_omega(self):
         assert_allclose([bubble.omega for bubble in self.bubbles], self.OMEGA_REF, rtol=1.3e-2)
@@ -56,7 +55,7 @@ class ThermoTestLectureNotes(ThermoTest, unittest.TestCase):
     # Reference values
     KAPPA_REF = np.array([0.189, 0.452, 0.235])
     OMEGA_REF = np.array([0.815, 0.559, 0.769])
-    KE_FRAC_REF = np.array([0.0172, 0.0411, 0.0213])
+    BVA_KE_FRAC_REF = np.array([0.0172, 0.0411, 0.0213])
     UBARFS_REF = np.array([0.119, 0.184, 0.133])
 
     def test_ubarf(self):
@@ -70,4 +69,4 @@ class ThermoTestHindmarshHijazi(ThermoTest, unittest.TestCase):
     # Reference values
     KAPPA_REF = np.array([0.610, 0.522, 0.264])
     OMEGA_REF = np.array([0.395, 0.491, 0.744])
-    KE_FRAC_REF = np.array([0.223, 0.0684, 0.022])
+    BVA_KE_FRAC_REF = np.array([0.223, 0.0684, 0.022])