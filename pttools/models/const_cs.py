Fix critical temperature calculation for ConstCSModel

pttools/models/const_cs.py
@@ -37,6 +37,7 @@ class ConstCSModel(AnalyticModel):
             t_min: float = None,
             t_max: float = None,
             t_ref: float = 1,
+            t_crit_guess: float = None,
             name: str = None,
             label_latex: str = None,
             label_unicode: str = None,
@@ -72,6 +73,9 @@ class ConstCSModel(AnalyticModel):
         self.t_ref = t_ref
         self.const_cs_wn_const: float = 4 / 3 * (1 / self.nu - 1 / self.mu)
 
+        if t_crit_guess is None:
+            t_crit_guess = t_ref
+
         label_prec = 3
         label_latex = f"Const. $c_s, c_{{ss}}^2={self.css2:.{label_prec}f}, c_{{sb}}^2={self.csb2:.{label_prec}f}$" \
             if not label_latex else label_latex
@@ -82,7 +86,7 @@ class ConstCSModel(AnalyticModel):
             V_s=V_s, V_b=V_b,
             a_s=a_s, a_b=a_b,
             g_s=g_s, g_b=g_b,
-            t_min=t_min, t_max=t_max,
+            t_min=t_min, t_max=t_max, t_crit_guess=t_crit_guess,
             name=name, label_latex=label_latex, label_unicode=label_unicode,
             allow_invalid=allow_invalid
         )
@@ -123,7 +127,7 @@ class ConstCSModel(AnalyticModel):
         invalid = ret < 0
         if (error_on_invalid or nan_on_invalid or log_invalid) and np.any(invalid):
             if np.isscalar(ret):
-                info = f"Got negative alpha_n={ret} with wn={wn}, mu={self.mu}, nu={self.nu}."
+                info = f"Got negative alpha_n={ret} with wn={wn}, mu={self.mu}, nu={self.nu}, t_crit={self.t_crit}."
             else:
                 i = np.argmin(wn)
                 info = f"Got negative alpha_n. Most problematic values: alpha_n={ret[i]}, wn={wn[i]}, mu={self.mu}, nu={self.nu}"

pttools/models/model.py
@@ -403,6 +403,8 @@ class Model(BaseModel, abc.ABC):
 
     def criticals(self, t_crit_guess: float, allow_fail: bool = False, log_info: bool = True):
         t_crit = self.critical_temp(guess=t_crit_guess, allow_fail=allow_fail)
+        # self.t_crit has to be set already here for alpha_n error messages to work.
+        self.t_crit = t_crit
         wn_min = self.w(t_crit, Phase.SYMMETRIC)
         alpha_n_min = self.alpha_n(wn_min)
 
@@ -722,6 +724,14 @@ class Model(BaseModel, abc.ABC):
                 wn = wn_sol[0][0]
 
         if not solution_found:
+            wn_sol = fsolve(self._w_n_solvable, x0=np.array([1]), args=(alpha_n, theta_bar), full_output=True)
+            solution_found = wn_sol[2] == 1
+            # reason = wn_sol[3]
+            if solution_found or np.isnan(wn):
+                logger.debug("w_n solution was found with wn_guess=1, but not wn_guess=%s", wn_guess)
+                wn = wn_sol[0][0]
+
+        if not solution_found:
             msg = (
                 f"w_n solution was not found for model={self.name}, "
                 f"alpha_n={alpha_n}, wn_guess={wn_guess}, theta_bar={theta_bar}. " +
@@ -754,9 +764,7 @@ class Model(BaseModel, abc.ABC):
         r"""Enthalpy at nucleation temperature with given $\alpha_n$"""
         # TODO: rename this to wn
         if wn_guess is None or np.isnan(wn_guess):
-            # TODO: fix this guess to be computed from the model parameters
-            # wn_guess = self.w_crit
-            wn_guess = 1
+            wn_guess = 1.01 * self.w_crit
 
         if np.isscalar(alpha_n):
             return self._w_n_scalar(