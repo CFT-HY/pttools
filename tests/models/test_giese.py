a2f7c842a14f336845ef66863c6c53b66014b542
Mika Mäki <mika.x.maki@helsinki.fi>
2023-09-26 18:31:43 +0300

    Run Giese tests for too small alpha_ns as well

--- tests/models/test_giese.py
+++ tests/models/test_giese.py
@@ -16,10 +16,9 @@ def assert_kappa(css2: float, csb2: float, kappa_ref: np.ndarray, rtol: float = 
     alpha_thetabar_ns = np.array([0.01, 0.1, 0.3])
     v_walls = np.linspace(0.2, 0.9, 8, endpoint=True)
     model = models.ConstCSModel(css2=css2, csb2=csb2, a_s=5, a_b=1, V_s=1)
-    kappas = np.zeros((alpha_thetabar_ns.size, v_walls.size))
-    bubbles, kappas[:, :] = create_bubbles(
+    bubbles, kappas = create_bubbles(
         model=model, v_walls=v_walls, alpha_ns=alpha_thetabar_ns,
-        func=get_kappa, bubble_kwargs={"theta_bar": True}, allow_bubble_failure=True)
+        func=get_kappa, bubble_kwargs={"theta_bar": True, "allow_invalid": True}, allow_bubble_failure=True)
 
     assert_allclose(kappas, kappa_ref, rtol=rtol, atol=atol)
 
@@ -38,6 +37,8 @@ def compare(
 
 
 def get_kappa(bubble: Bubble) -> float:
+    if not bubble.solved:
+        return np.nan
     return bubble.kappa
 
 
@@ -67,45 +68,40 @@ class GieseTest(unittest.TestCase):
         compare(model, alpha_ns, v_walls, kappa_ref, rtol=0.085)
 
     @staticmethod
-    @unittest.expectedFailure
     def test_kappa33():
-        kappa_ref = np.array([[0.00741574, 0.01450964, 0.02653822, 0.05782794, 0.18993211,
-        0.04904255, 0.0265001 , 0.01816217],
-       [0.06921508, 0.12306195, 0.18896878, 0.28120044, 0.41124208,
-        0.44902447, 0.23408969, 0.15686063],
-       [0.18355195, 0.28898783, 0.38244706, 0.47270278, 0.56546988,
-        0.6231619 , 0.57442573, 0.36606867]])
-        assert_kappa(css2=1 / 3, csb2=1 / 3, kappa_ref=kappa_ref, rtol=7.3e-3)
+        kappa_ref = np.array([
+            [0.00741574, 0.01450964, 0.02653822, 0.05782794, 0.18993211, 0.04904255, 0.0265001, 0.01816217],
+            [0.06921508, 0.12306195, 0.18896878, 0.28120044, 0.41124208, 0.44902447, 0.23408969, 0.15686063],
+            [0.18355195, 0.28898783, 0.38244706, 0.47270278, 0.56546988, 0.6231619, 0.57442573, 0.36606867]
+        ])
+        assert_kappa(css2=1/3, csb2=1/3, kappa_ref=kappa_ref, rtol=7.3e-3)
 
     @staticmethod
     @unittest.expectedFailure
     def test_kappa34():
-        kappa_ref = np.array([[0.00709227, 0.01341795, 0.02375007, 0.05032617, 0.04033708,
-        0.01933255, 0.01273443, 0.00937875],
-       [0.06570444, 0.11244944, 0.16679036, 0.24170456, 0.28645206,
-        0.18837795, 0.11564567, 0.0850799 ],
-       [0.17210839, 0.25999769, 0.3327127 , 0.40143091, 0.44930404,
-        0.4318333 , 0.29546582, 0.21279852]])
-        assert_kappa(css2=1 / 3, csb2=1 / 4, kappa_ref=kappa_ref, rtol=0.6)
+        kappa_ref = np.array([
+            [0.00709227, 0.01341795, 0.02375007, 0.05032617, 0.04033708, 0.01933255, 0.01273443, 0.00937875],
+            [0.06570444, 0.11244944, 0.16679036, 0.24170456, 0.28645206, 0.18837795, 0.11564567, 0.0850799],
+            [0.17210839, 0.25999769, 0.3327127, 0.40143091, 0.44930404, 0.4318333, 0.29546582, 0.21279852]
+        ])
+        assert_kappa(css2=1/3, csb2=1/4, kappa_ref=kappa_ref, rtol=0.61)
 
     @staticmethod
     @unittest.expectedFailure
     def test_kappa43():
-        kappa_ref = np.array([[0.00730452, 0.01543951, 0.0342464 , 0.1244067 ,        np.nan,
-        0.04904255, 0.0265001 , 0.01816217],
-       [0.06740045, 0.12497735, 0.2058207 , 0.32704976, 0.47932218,
-        0.50230337, 0.23408969, 0.15686063],
-       [0.17642267, 0.28297756, 0.38474954, 0.48622994, 0.58952004,
-        0.65276938, 0.59256005, 0.36606867]])
-        assert_kappa(css2=1 / 4, csb2=1 / 3, kappa_ref=kappa_ref, rtol=0.27)
+        kappa_ref = np.array([
+            [0.00730452, 0.01543951, 0.0342464, 0.1244067, np.nan, 0.04904255, 0.0265001, 0.01816217],
+            [0.06740045, 0.12497735, 0.2058207, 0.32704976, 0.47932218, 0.50230337, 0.23408969, 0.15686063],
+            [0.17642267, 0.28297756, 0.38474954, 0.48622994, 0.58952004, 0.65276938, 0.59256005, 0.36606867]
+        ])
+        assert_kappa(css2=1/4, csb2=1/3, kappa_ref=kappa_ref, rtol=0.29)
 
     @staticmethod
     @unittest.expectedFailure
     def test_kappa44():
-        kappa_ref = np.array([[0.00700235, 0.01434407, 0.03086914, 0.11071288, 0.04033708,
-        0.01933255, 0.01273443, 0.00937875],
-       [0.06410723, 0.11460043, 0.18265583, 0.28383528, 0.34206562,
-        0.18837795, 0.11564567, 0.0850799 ],
-       [0.16570685, 0.25535541, 0.33629884, 0.41548919, 0.4724849 ,
-        0.45536877, 0.29546582, 0.21279852]])
-        assert_kappa(css2=1 / 4, csb2=1 / 4, kappa_ref=kappa_ref, rtol=0.62)
+        kappa_ref = np.array([
+            [0.00700235, 0.01434407, 0.03086914, 0.11071288, 0.04033708, 0.01933255, 0.01273443, 0.00937875],
+            [0.06410723, 0.11460043, 0.18265583, 0.28383528, 0.34206562, 0.18837795, 0.11564567, 0.0850799],
+            [0.16570685, 0.25535541, 0.33629884, 0.41548919, 0.4724849, 0.45536877, 0.29546582, 0.21279852]
+        ])
+        assert_kappa(css2=1/4, csb2=1/4, kappa_ref=kappa_ref, rtol=0.62)
