67006bf7383b6a5065c7b6ba5f5bc9997f0bee44
Mika Mäki <mika.x.maki@helsinki.fi>
2023-12-04 16:20:52 +0200

    Fix temperature range for model comparison plots

--- examples/plot_model_comparison.py
+++ examples/plot_model_comparison.py
@@ -3,7 +3,6 @@ Comparison of BagModel and ConstCSModel
 =======================================
 """
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from examples import utils
@@ -14,10 +13,10 @@ from pttools import models
 def main():
     model_bag = models.BagModel(a_s=1.1, a_b=1, V_s=1)
     # model_const_cs_like_bag = models.ConstCSModel(a_s=1.1, a_b=1, css2=1/3, csb2=1/3, V_s=1)
-    model_thermo_bag = models.FullModel(thermo=models.ConstCSThermoModel(a_s=1.1, a_b=1, V_s=1, css2=1/3.3, csb2=1/3))
+    model_thermo_bag = models.FullModel(thermo=models.ConstCSThermoModel(a_s=1.1, a_b=1, V_s=1, css2=1/3, csb2=1/3))
     model_sm = models.FullModel(thermo=models.StandardModel(), allow_invalid=True)
 
-    plot = ModelsPlot(temp=np.logspace(1, 3, 100))
+    plot = ModelsPlot(temp=np.logspace(1, 2.5, 100))
 
     plot.add(model_bag, phase=models.Phase.SYMMETRIC)
     plot.add(model_thermo_bag, phase=models.Phase.SYMMETRIC, ls="--")
