46baf65b9ae158bbcd12b2648dbce7db8a68150a
Mika Mäki <mika.x.maki@helsinki.fi>
2024-09-24 21:32:52 +0300

    Add additional curves to the const_cs_gw example

--- examples/const_cs_gw.py
+++ examples/const_cs_gw.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from examples import utils
-# pttools.bubble import Bubble
+from pttools.bubble import lorentz
+from pttools.bubble.shock import solve_shock
 from pttools.models import ConstCSModel
 from pttools.ssmtools import Spectrum
 from pttools.analysis.parallel import create_spectra
@@ -35,6 +36,8 @@ def main():
         ConstCSModel(css2=1/4, csb2=1/3, a_s=a_s, a_b=a_b, V_s=V_s, alpha_n_min=alpha_n_min, allow_invalid=allow_invalid),
         ConstCSModel(css2=1/4, csb2=1/4, a_s=a_s, a_b=a_b, V_s=V_s, alpha_n_min=alpha_n_min, allow_invalid=allow_invalid),
     ]
+    css2s = {model.css2 for model in models}
+    csb2s = {model.csb2 for model in models}
     alpha_n_mins = np.array([model.alpha_n_min for model in models])
     if np.any(alpha_n_mins > alpha_n_min):
         msg = f"A model has alpha_n_min > {alpha_n_min}. Please adjust the models. Currently alpha_n_mins={alpha_n_mins}",
@@ -67,11 +70,46 @@ def main():
             ax.set_ylabel(r"$\mathcal{P}_{\text{gw}}(z)$")
             title = rf"$\alpha_n={alpha_n}, v_\text{{wall}}={v_wall}$"
             ax.set_title(title)
-            ax.legend()
 
             ax2.set_xlabel(r"$\xi$")
             ax2.set_ylabel(r"$v(\xi)$")
             ax2.set_title(title)
+
+    # Mu curves
+    for csb2 in csb2s:
+        csb = np.sqrt(csb2)
+        xi_mu: np.ndarray = np.linspace(csb, 1, 20)
+        v_mu = lorentz(xi=xi_mu, v=csb)
+        for ax in axs2.flat:
+            ax.plot(xi_mu, v_mu, ls=":", c="k")
+
+    for ax in axs2.flat:
+        ax.set_xlim(0.3, 0.7)
+        ax.set_ylim(0, 0.4)
+
+    # Shock surfaces
+    n_xi = 20
+    for model in models:
+        xi_arr: np.ndarray = np.linspace(model.css, 0.99, n_xi)
+        for i_alpha_n, alpha_n in enumerate(alpha_ns):
+            vm_arr: np.ndarray = np.zeros_like(xi_arr)
+            for i_xi, xi in enumerate(xi_arr):
+                wn = model.w_n(alpha_n=alpha_n)
+                vm_tilde, wm = solve_shock(model, v1_tilde=xi, w1=wn, backwards=True, warn_if_barely_exists=False)
+                vm_arr[i_xi] = lorentz(xi, vm_tilde)
+
+            filter_arr = np.logical_or(vm_arr > 1, vm_arr <= 0)
+            # Do not filter the first point
+            filter_arr[0] = 0
+            vm_arr[filter_arr] = np.nan
+            for i_v_wall, v_wall in enumerate(v_walls):
+                ax = axs2[i_v_wall, i_alpha_n]
+                ax.plot(xi_arr, vm_arr, color="k")
+
+    for ax in axs.flat:
+        ax.legend()
+    for ax in axs2.flat:
+        ax.legend()
 
     fig.tight_layout()
     fig2.tight_layout()
