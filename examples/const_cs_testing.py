9be1f8842175e0ffdf4f52f8441b2d903b8668a6
Mika Mäki <agentti002x@gmail.com>
2024-12-03 17:08:03 +0200

    Improve const_cs example

--- examples/const_cs_testing.py
+++ examples/const_cs_testing.py
@@ -5,9 +5,23 @@ ConstCSModel testing
 Simple testing for the constant sound speed model
 """
 
-from pttools.models.const_cs import ConstCSModel
+import matplotlib.pyplot as plt
 
+from pttools.bubble import Bubble
+from pttools.models import ConstCSModel
+from pttools.omgw0 import Spectrum
 
-model = ConstCSModel(1/3, 1/4, V_s=5, a_s=10, a_b=5)
-print(model.T_crit)
-# print(model.alpha_n(552))
+# Specify the equation of state
+const_cs = ConstCSModel(
+    a_s=1.5, a_b=1, css2=1/3, csb2=1/3-0.01, V_s=1
+)
+
+# Create a bubble and solve its fluid profile
+bubble = Bubble(const_cs, v_wall=0.5, alpha_n=0.2)
+bubble.plot()
+
+# Compute gravitational wave spectrum for the bubble
+spectrum = Spectrum(bubble)
+spectrum.plot_multi()
+
+plt.show()
