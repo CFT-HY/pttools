2f6e4dba35790efcb57dd550924f6d00f805e127
Mika Mäki <mika.x.maki@helsinki.fi>
2025-02-06 14:32:41 +0200

    Improve Giese et al. code import

--- pttools/bubble/giese.py
+++ pttools/bubble/giese.py
@@ -1,11 +1,20 @@
-# import logging
+import logging
+import os
+import sys
 import typing as tp
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 try:
-    # logger.info("Giese code imported successfully.")
     from giese.lisa import kappaNuMuModel
 except ImportError:
-    # logger.info("Giese could not be imported.")
+    pttools_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    parent_dir = os.path.dirname(pttools_path)
+    msc_python_path = os.path.join(parent_dir, "msc-thesis2", "msc2-python")
+    if os.path.isdir(msc_python_path):
+        sys.path.append(msc_python_path)
+        try:
+            from giese.lisa import kappaNuMuModel
+        except ImportError:
+            logger.error("The msc-thesis2 repository was found but the Giese et al. code could not be imported.")
     kappaNuMuModel: tp.Optional[callable] = None
