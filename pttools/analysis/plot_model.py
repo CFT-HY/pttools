0944d9f95b0fac0120417b95b26695a7a2d9c324
Mika Mäki <mika.x.maki@helsinki.fi>
2025-08-28 11:08:37 +0200

    Improve ModelPlot

--- pttools/analysis/plot_model.py
+++ pttools/analysis/plot_model.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from pttools import models
+from pttools.analysis.utils import A4_PAPER_SIZE
 from pttools.bubble import Phase
 
 logger = logging.getLogger(__name__)
@@ -25,7 +26,7 @@ class ModelPlot:
         self.model = model
         self.t_log = t_log
 
-        self.fig: plt.Figure = plt.figure(figsize=(11.69, 8.27))
+        self.fig: plt.Figure = plt.figure(figsize=A4_PAPER_SIZE)
         self.axs = self.fig.subplots(nrows=3, ncols=3)
         self.ax_p: plt.Axes = self.axs[0, 0]
         self.ax_s: plt.Axes = self.axs[0, 1]
@@ -51,18 +52,20 @@ class ModelPlot:
         self.plot(self.ax_e, self.model.e_temp, "e", y_log=y_log)
         self.plot(
             self.ax_cs2, self.model.cs2_temp,
-            label="c_s^2", label_s="$c_{s,s}^2$", label_b="$c_{s,b}^2$", y_lim=False, y_log=False)
-
+            label="c_s^2", label_s="$c_{s,s}^2$", label_b="$c_{s,b}^2$", y_lim=False, y_log=False
+        )
         self.fig.tight_layout()
 
     def plot(
             self,
-            ax: plt.Axes, func: tp.Callable,
+            ax: plt.Axes,
+            func: tp.Callable,
             label: str | None = None,
             label_s: str | None = None,
             label_b: str | None = None,
             y_lim: bool = True,
-            y_log: bool = True):
+            y_log: bool = True,
+            ticks: bool = True):
         if label_s is None and label is not None:
             label_s = f"${label}_s$"
         if label_b is None and label is not None:
@@ -85,7 +88,7 @@ class ModelPlot:
         ax.plot(self.temps_b, vals_b_low, color="b", label=label_b)
         ax.plot(self.temps_s, vals_b_high, color="b", ls=":")
 
-        ax.axvline(self.model.T_crit, ls=":", label=r"$T_{crit}$")
+        ax.axvline(self.model.T_crit, ls="-.", label=r"$T_{crit}$", color="g")
         ax.set_xlabel("$T$")
         ax.set_ylabel(f"${label}$")
 
@@ -106,4 +109,7 @@ class ModelPlot:
             ax.set_xscale("log")
         if y_log:
             ax.set_yscale("log")
+        if not ticks:
+            ax.get_xaxis().set_ticks([])
+            ax.get_yaxis().set_ticks([])
         ax.legend()
