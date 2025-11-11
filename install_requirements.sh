0acc0dd34179ee8a27a01ee6eff3a329da32a0d9
Mika Mäki <mika.x.maki@helsinki.fi>
2025-11-12 01:13:40 +0200

    Improve wheel installation

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,4 +2,5 @@
 set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pip install --upgrade pip wheel
 pip install --upgrade -r "${SCRIPT_DIR}/requirements.txt" -r "${SCRIPT_DIR}/requirements-dev.txt" -r "${SCRIPT_DIR}/docs/requirements.txt"
