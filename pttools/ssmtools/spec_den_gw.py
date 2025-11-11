Fix eps_lookup handling for spectrum computation and improve its robustness

pttools/ssmtools/spec_den_gw.py
@@ -1,5 +1,6 @@
 """Functions for computing the spectral density of the gravitational waves"""
 
+# import logging
 import typing as tp
 
 import numba
@@ -9,6 +10,8 @@ import numpy as np
 import pttools.type_hints as th
 from pttools import speedup
 from pttools.ssmtools import const
+
+# logger = logging.getLogger(__name__)
 
 
 @numba.njit
@@ -21,6 +24,19 @@ def gen_lookup(y: np.ndarray, cs: float, n_z_lookup: int = const.N_Z_LOOKUP_DEFA
     :return: Generated lookup array for z
     """
     z_minus_min, z_plus_max = lookup_limits(y, cs, eps)
+    # If the eps were summed instead of multiplied, then we would have to check for negative z_minus_min.
+    # if z_minus_min <= 0:
+    #     z_minus_min_old = z_minus_min
+    #     eps_old = eps
+    #     while z_minus_min <= 0:
+    #         eps *= 0.1
+    #         z_minus_min, z_plus_max = lookup_limits(y, cs, eps)
+    #     with numba.objmode:
+    #         logger.warning(
+    #             "Got z_minus_min=%s <= 0 with eps=%s. Recomputed to %s with eps=%s.",
+    #             z_minus_min_old, eps_old, z_minus_min, eps
+    #         )
+
     # The variable to integrate over in eq. 3.44 and 3.47
     return speedup.logspace(np.log10(z_minus_min), np.log10(z_plus_max), n_z_lookup)
 
@@ -28,7 +44,9 @@ def gen_lookup(y: np.ndarray, cs: float, n_z_lookup: int = const.N_Z_LOOKUP_DEFA
 @numba.njit
 def lookup_limits(y: np.ndarray, cs: float, eps: float = 0.) -> tuple[float, float]:
     """Defined on p. 12 between eq. 3.44 and 3.45"""
-    return y.min() * 0.5 * (1. - cs) / cs - eps, y.max() * 0.5 * (1. + cs) / cs + eps
+    z_minus_min = y.min() * 0.5 * (1. - cs) / cs * (1 - eps)
+    z_plus_max = y.max() * 0.5 * (1. + cs) / cs * (1 + eps)
+    return z_minus_min, z_plus_max
 
 
 @numba.njit(parallel=True, nogil=True)

pttools/ssmtools/spec_den_v.py
@@ -82,7 +82,14 @@ def spec_den_v(
     log10tmin = np.log10(tmin)
     log10tmax = np.log10(tmax)
 
-    qT_lookup = 10 ** np.arange(log10zmin + log10tmin, log10zmax + log10tmax, dlog10z)
+    try:
+        qT_lookup = 10 ** np.arange(log10zmin + log10tmin, log10zmax + log10tmax, dlog10z)
+    except ValueError as e:
+        logger.error(
+            "Could not compute qT_lookup with log10zmin=%s, log10tmin=%s, log10zmax=%s, log10tmax=%s, dlog10z=%s",
+            log10zmin, log10tmin, log10zmax, log10tmax, dlog10z
+        )
+        raise e
     A2_lookup = ssm.a2_e_conserving(bub=bub, z=qT_lookup, cs=cs, z_st_thresh=z_st_thresh)[0]
     # if qT_lookup.size != A2_lookup.size:
     #     raise ValueError(f"Lookup sizes don't match: {qT_lookup.size} != {A2_lookup.size}")

pttools/ssmtools/spectrum.py
@@ -48,12 +48,18 @@ class SSMSpectrum:
         :param lifetime_multiplier: used for computing the source lifetime factor
         :param compute: whether to compute the spectrum immediately
         """
+        if y is None:
+            self.y = const.Y_DEFAULT
+        elif np.isnan(y).any():
+            raise ValueError("y must not contain nan values.")
+        else:
+            self.y = y
+
         # Parameters
         self.bubble = bubble
         # self.de_method = de_method
         # self.method = method
         self.nuc_type = nuc_type
-        self.y = const.Y_DEFAULT if y is None else y
         self.z_st_thresh = z_st_thresh
         self.nt = nt
         self.n_z_lookup = n_z_lookup
@@ -126,20 +132,20 @@ class SSMSpectrum:
         """
         return (8*np.pi)**(1/3) * self.bubble.v_wall / self.r_star
 
-    def compute(self):
+    def compute(self, eps_lookup: float = 1e-8, lifetime_distribution_a: float = 1.):
         if not self.bubble.solved:
             self.bubble.solve()
 
         self.cs = np.sqrt(self.bubble.model.cs2(self.bubble.va_enthalpy_density, Phase.BROKEN))
         self.spec_den_v, self.a2 = spec_den_v(
-            bub=self.bubble, z=self.y, a=1.,
+            bub=self.bubble, z=self.y, a=lifetime_distribution_a,
             nuc_type=self.nuc_type, nt=self.nt, z_st_thresh=self.z_st_thresh, cs=self.cs, return_a2=True
         )
         self.pow_v = pow_spec(self.y, spec_den=self.spec_den_v)
 
-        self.z_lookup = gen_lookup(y=self.y, cs=self.cs, n_z_lookup=self.n_z_lookup, eps=1e-8)
+        self.z_lookup = gen_lookup(y=self.y, cs=self.cs, n_z_lookup=self.n_z_lookup, eps=eps_lookup)
         self.spec_den_v_lookup = spec_den_v(
-            bub=self.bubble, z=self.z_lookup, a=1.,
+            bub=self.bubble, z=self.z_lookup, a=lifetime_distribution_a,
             nuc_type=self.nuc_type, nt=self.nt, z_st_thresh=self.z_st_thresh, cs=self.cs
         )
         self.spec_den_gw, y = spec_den_gw_scaled(