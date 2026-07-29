7113ed870f3ae1d72dc0fb697df5b7b71f9b2613
Mika Mäki <mika.x.maki@helsinki.fi>
2026-07-29 18:54:29 +0300

    Add a link to pttools_omgw0_addons

--- pttools/omgw0/__init__.py
+++ pttools/omgw0/__init__.py
@@ -1,6 +1,9 @@
 r"""
 Functions for calculating the gravitational wave power spectrum $\Omega_{\text{gw}}(f)$
 as a function of physical frequency $f$ in the universe today
+
+An earlier implementation by Chloe Hopling is available in
+`pttools_omgw0_addons <https://bitbucket.org/cgowling/pttools_omgw0_addons/>`_.
 """
 
 from .approx import *
