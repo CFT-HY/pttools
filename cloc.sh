9aa6596fdcd14c689bdc954be2792ef6d3e99529
Mika Mäki <mika.x.maki@helsinki.fi>
2026-08-12 00:30:31 +0300

    Fix cloc usage

--- cloc.sh
+++ cloc.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
-set -e
+set -eu
 
 # Count lines of code in the project
 
-cloc --by-file-by-lang --exclude-list-file=.clocignore .
+# https://github.com/AlDanial/cloc/issues/784
+# cloc --by-file-by-lang --exclude-list-file=.clocignore .
+# https://stackoverflow.com/a/26679008
+# cloc --by-file-by-lang --exclude-dir=$(tr '\n' ',' < .clocignore) .
+# This uses .gitignore
+cloc --by-file-by-lang --vcs=git .
