8e906fd5706436aac01866f71767b97cbcf3c825
Mika Mäki <mika.x.maki@helsinki.fi>
2026-09-21 02:57:36 +0300

    Fix LaTeX typo in ConstCSModel.alpha_theta_bar_plus docstring

--- pttools/models/const_cs.py
+++ pttools/models/const_cs.py
@@ -550,9 +550,9 @@ class ConstCSModel(AnalyticModel):
             error_on_invalid: bool = True,
             nan_on_invalid: bool = True,
             log_invalid: bool = True) -> T:
-        r"""Transition strength parameter $\alpha_{\bar{\theta}_+$.
+        r"""Transition strength parameter $\alpha_{\bar{\theta}_+}$.
 
-        $$\alpha_{\bar{\theta}_+
+        $$\alpha_{\bar{\theta}_+}
         = \frac{1}{3} \left( 1 - \frac{\mu_-}{\mu_+} \right) + \frac{\mu_-}{4} \alpha_{+,\text{bag}}$$
         :maki_msc:`\ ` eq. 2.137
         """
